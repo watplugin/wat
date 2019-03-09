9e5d227aa6e9000fdfe5a1c174457039a1dc6b90
Darigan <darigan@protonmail.com>
2019-03-09 12:11:23 +0000

    Add is_equal tests to test_primitives.gd

--- addons/WAT/Scripts/expectations.gd
+++ addons/WAT/Scripts/expectations.gd
@@ -3,9 +3,13 @@ class_name Expectations
 
 signal OUTPUT
 
-func output(success: bool, message: String):
+func output(success: bool, message: String) -> void:
 	emit_signal("OUTPUT", success, message)
 	
-func is_true(condition: bool, message: String):
+func is_true(condition: bool, message: String) -> void:
 	# We'll expand on these later but this should be fine now
 	output(condition, message)
+	
+func is_equal(a, b, message: String) -> void:
+	# May need to add a typeof check here
+	output((a == b), message)
--- tests/test_primitives.gd
+++ tests/test_primitives.gd
@@ -1,5 +1,12 @@
 extends WATT
 
+### BEGIN NOTES ###
+# All of these tests are standalone expressions so some of what they're doing doesn't look
+# immediatly obvious. In this case, when writing a failing test, we write the message as "x equals x"
+# whereas the expectation is written as x against y. What we're essentially doing here is pretending
+# that Y is a wrong "return value". 
+### END NOTES ###
+
 func test_expect_is_true_all_of_these_should_pass():
 	expect.is_true((true), "true is true")
 	expect.is_true((true != false), "true is not false")
@@ -24,3 +31,27 @@ func test_expect_is_true_all_of_these_should_fail():
 	expect.is_true((bad.vec3 is Vector3), "Vector3(0, 0, 0) is Vector3")
 	expect.is_true((bad.array is Array), "[] is Array")
 	expect.is_true((bad.dict is Dictionary), "{} is Dictionary")
+	
+func test_expect_is_equal_all_of_these_should_pass():
+	# Add Exceptions for this comparisons due to unusual nature
+	# expect.is_equal({}, {}, "{} equals {}") # Is inequal // Add a Warning about Dictionary Exceptions
+	# expect.is_equal(false, null, "false equals null") # Is inequal
+	expect.is_equal(true, true, "true equals true")
+	expect.is_equal(false, false, "false equals false")
+	expect.is_equal(1, 1, "1 equals 1")
+	expect.is_equal(1.0, 1.0, "1.0 equals 1.0")
+	expect.is_equal("String", "String", '"String" equals "String"')
+	expect.is_equal([], [], "[] equals []")
+	expect.is_equal(Vector2(0, 0), Vector2(0, 0), "Vector2(0, 0) equals Vector2(0, 0)")
+	expect.is_equal(Vector3(0, 0, 0), Vector3(0, 0, 0), "Vector3(0, 0, 0) equals Vector3(0, 0, 0)")
+	
+func test_expect_is_equal_all_of_these_should_fail():
+	# Add Notes for these comparisons due to implicit conversion
+	# expect.is_equal(1, 1.0, "1 equals 1")
+	# expect.is_equal(1.0, 1, "1.0 equals 1.0")
+	expect.is_equal(true, false, "true equals true")
+	expect.is_equal(false, true, "false equals false")
+	expect.is_equal("String", "Words", '"String" equals "String"')
+	expect.is_equal([], [1, 2, 3], "[] equals []")
+	expect.is_equal(Vector2(0, 0), Vector2(10, 10), "Vector2(0, 0) equals Vector2(0, 0)")
+	expect.is_equal(Vector3(0, 0, 0), Vector3(10, 10, 10), "Vector3(0, 0, 0) equals Vector3(0, 0, 0)")
