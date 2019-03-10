53777615e4476b9d5df2d4923ed52fd492f6875e
Darigan <darigan@protonmail.com>
2019-03-10 12:57:52 +0000

    Add is greater than expectation
    - Added some more constants
    - Added a type check for collections

--- addons/WAT/Scripts/expectations.gd
+++ addons/WAT/Scripts/expectations.gd
@@ -4,6 +4,11 @@ class_name Expectations
 class OP:
 	const EQUAL = "=="
 	const INEQUAL = "!="
+	const GREATER = ">"
+	const LESSER = "<"
+	const GREATER_THAN_OR_EQUAL = "=>"
+	const LESS_THAN_OR_EQUAL = "<="
+	const IS = "is"
 
 signal OUTPUT
 
@@ -27,6 +32,16 @@ func is_not_equal(a, b, message: String) -> void:
 	var operator: String = OP.INEQUAL if success else OP.EQUAL
 	var result: String = "%s %s %s" % [_stringify(a), operator, _stringify(b)]
 	output((a != b), message, result)
+	
+func is_greater_than(a, b, message: String) -> void:
+	var success: bool
+	if a is Dictionary or a is Array:
+		success = a.size() > b.size()
+	else:
+		success = a > b
+	var operator: String = OP.GREATER if success else OP.LESS_THAN_OR_EQUAL
+	var result: String = "%s %s %s" %[_stringify(a), operator, _stringify(b)]
+	output(success, message, result)
 	
 func _stringify(variable) -> String:
 	return "< %s | %s >" % [BuiltIn.to_string(variable).to_upper(), str(variable)]
