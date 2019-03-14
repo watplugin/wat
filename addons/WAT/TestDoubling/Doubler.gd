bc8810ba8ee4ee7a99cc6ecae29d88b559f0acc8
Darigan <darigan@protonmail.com>
2019-03-14 13:32:26 +0000

    Implement Doubler Stub Method
    A bit messy but not a lot of code so we can handle soem refactoring later.

--- addons/WAT/TestDoubling/Doubler.gd
+++ addons/WAT/TestDoubling/Doubler.gd
@@ -4,17 +4,59 @@ extends Node
 class_name WATDouble
 
 ####	# Add self as metadata to script (we could do a property object but using metadata is more inconspicoius)
-#### Requirements? (Not set in stone)
-#### Doubler - Delegates to others
-#### Writer - Reads/Write saves scripts (probably to a user://WATemp folder
 #### Config for type checking (do we keep as is, remove param/return types or mod elsewhere?)
-#### Handle variables (like constants etc)
 #### Add a Method Class, a Call class (or data structure?)
 
 const _WRITER = preload("res://addons/WAT/TestDoubling/Writer.gd")
 var instance
-var methods: Dictionary = {}
+var _methods: Dictionary = {}
 
 func _init(script: Script) -> void:
-	var instance = _WRITER.new().rewrite(script)
-	print(instance)
+	self.instance = _WRITER.new().rewrite(script)
+	instance.set_meta("double", self)
+
+
+func get_retval(id: String, arguments: Dictionary):
+	if not _methods.has(id):
+		_methods[id] = Method.new(id, 1, arguments)
+		return null
+	else:
+		var retval = _methods[id].get_retval(arguments)
+		print(retval)
+		return retval
+#		return _methods[id].get_retval(arguments)
+
+func stub(id: String, arguments: Dictionary, retval) -> void:
+	if not _methods.has(id):
+		_methods[id] = Method.new(id, 0)
+	_methods[id].stub(arguments, retval)
+
+class Method:
+	var identifier: String
+	var calls: Array = []
+	var stubs: Array = []
+	var call_count: int = 0
+
+	func _init(method_id, call: int, arguments: Dictionary = {}) -> void:
+		self.identifier = method_id
+		call_count += call
+		if call_count != 0:
+			calls.append(arguments)
+			
+	func stub(arguments: Dictionary, retval):
+		stubs.append({"arguments": arguments, "retval": retval})
+
+	func get_retval(arguments: Dictionary):
+		var retval
+		for stub in stubs:
+			retval = stub.retval
+			print("retval: ", stub.retval)
+			var correct: bool = true
+			for key in arguments:
+				if arguments[key] != stub.arguments[key]:
+					correct = false
+					break # set flag here?
+			if correct:
+				break
+		print(retval)
+		return retval
--- tests/test_doubler.gd
+++ tests/test_doubler.gd
@@ -4,6 +4,11 @@ var double_register: WATDouble
 
 func _pre():
 	double_register = WATDouble.new(TestTest)
+	double_register.stub("login", {"username": "alex", "password": "code"}, "Stub is Correct")
+	double_register.stub("register", {"username": "Jack", "password": "hit"}, "Hello?")
 	
 func test_doubler_exists():
 	expect.is_true(double_register != null, "double exists")
+	expect.is_equal("Stub is Correct", double_register.instance.login("alex", "code"), "100 == 100")
+	expect.is_equal("Hello?", double_register.instance.register("alex", "hit"), "Hello? == Hello?")
+	expect.is_equal("Beep Beep", "Beep Beep", "Beep Beep == Beep Beep")
