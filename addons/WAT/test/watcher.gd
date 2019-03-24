9a0c53d95d592eafe2cbf22454d5ad75d1b6e005
Darigan <darigan@protonmail.com>
2019-03-24 01:39:09 +0000

    Remove string typing from event variable
    No "any" equivelant and it can be String or Null.

--- addons/WAT/test/watcher.gd
+++ addons/WAT/test/watcher.gd
@@ -8,7 +8,7 @@ func watch(emitter, event: String) -> void:
 
 func _add_emit(a = null, b = null, c = null, d = null, e = null, f = null, g = null, h = null, i = null, j = null, k = null):
 	var arguments: Array = [a, b, c, d, e, f, g, h, i, j, k]
-	var event: String
+	var event
 	while not event:
 		event = arguments.pop_back()
 	var obj: String = arguments.pop_back()
