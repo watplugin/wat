9d24e4b3daaba22be873adf809da3de930d3670e
Darigan <darigan@protonmail.com>
2019-03-24 01:30:14 +0000

    Move saving/loading/instance out of rewriter to parent doubler class.
    We might require splitting this up further soon as well.

--- addons/WAT/double/double.gd
+++ addons/WAT/double/double.gd
@@ -1,8 +1,14 @@
 extends Node
 class_name WATDouble
 
+# Should probably split this into three; 
+# One for save/load
+# one for method managing
+# one for middle-man class? 
+const _TEMP: String = "user://WATemp/"
 const TOKENIZER = preload("res://addons/WAT/double/tokenizer.gd")
 const REWRITER = preload("res://addons/WAT/double/rewriter.gd")
+const SCRIPT = preload("res://addons/WAT/double/Blank.gd")
 const METHOD = preload("method.gd")
 var _methods: Dictionary = {}
 var instance: Object # May need to change if doubling scenes?
@@ -11,8 +17,23 @@ func _init(script):
 	var source = TOKENIZER.start(script)
 	for method in source.methods:
 		_add_method(method.name)
-	instance = REWRITER.start(source)
+	var doubled_source: String = REWRITER.start(source)
+	_save(source.title, doubled_source)
+	instance = _load(source.title)
 	instance.set_meta("double", self)
+	
+static func _load(title: String) -> Object:
+	return load("%s%s.gd" % [_TEMP, title]).new()
+	
+static func _save(title: String, rewrite: String) -> void:
+	_create_directory()
+	SCRIPT.source_code = rewrite
+	ResourceSaver.save("%s%s.gd" % [_TEMP, title], SCRIPT)
+
+static func _create_directory() -> void:
+	var dir = Directory.new()
+	if not dir.dir_exists(_TEMP):
+		dir.make_dir(_TEMP)
 
 func _add_method(_name: String) -> void:
 	var method: METHOD = METHOD.new(_name)
--- addons/WAT/double/rewriter.gd
+++ addons/WAT/double/rewriter.gd
@@ -1,16 +1,17 @@
 extends Reference
 
-const _TEMP: String = "user://WATemp/"
 
-static func start(source: Object) -> Script:
+static func start(source: Object) -> String:
 	var rewrite: String
 	rewrite += "%s" % source.extend
 	for method in source.methods:
 		rewrite += _rewrite_method(method)
-	_save(source.title, rewrite)
-	var x = load(_TEMP + source.title + ".gd").new()
-	return x
-#	return load(_TEMP + source.title + ".gd")
+	return rewrite
+	
+	#	_save(source.title, rewrite)
+#	var x = load(_TEMP + source.title + ".gd").new()
+#	return x
+##	return load(_TEMP + source.title + ".gd")
 	
 static func _rewrite_method(method: Dictionary) -> String:
 	var rewritten_method: String = "func %s(%s)%s:%s"
@@ -45,17 +46,16 @@ static func _rewrite_body(title, parameters) -> String:
 	var retval: String = '\n\treturn self.get_meta("double").get_retval("%s", arguments)\n\n' % title
 	return args + retval
 
-static func _save(title: String, rewrite: String) -> void:
-	_create_directory()
-	var file: File = File.new()
-	file.open("%s%s.gd" % [_TEMP, title], file.WRITE)
-	file.store_string(rewrite)
-	file.close()
-	
-static func _create_directory() -> void:
-	var dir = Directory.new()
-	if not dir.dir_exists(_TEMP):
-		dir.make_dir(_TEMP)
+#static func _save(title: String, rewrite: String) -> void:
+#	_create_directory()
+#	var script = load("res://addons/WAT/double/Blank.gd")
+#	script.source_code = rewrite
+#	ResourceSaver.save("%s%s.gd" % [_TEMP, title], script)
+#
+#static func _create_directory() -> void:
+#	var dir = Directory.new()
+#	if not dir.dir_exists(_TEMP):
+#		dir.make_dir(_TEMP)
 # write
 # save
 #func _instance(source: Source) -> Script:
