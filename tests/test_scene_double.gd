b919c43b9865a402d83169aec931835f6d246269
Darigan <darigan@protonmail.com>
2019-03-28 17:27:12 +0000

    remove a lone print debug

--- tests/test_scene_double.gd
+++ tests/test_scene_double.gd
@@ -6,6 +6,5 @@ func test_double_scene():
 	var scene_double = WATDouble.scene(scene)
 	add_child(scene_double.instance)
 	scene_double.instance.get_node("A").execute()
-#	scene_double.instance.test()
 	expect.was_called(scene_double.instance.get_node("A").get_meta("double"), "execute", "execute was called")
 	expect.was_called(scene_double.instance.get_meta("double"), "test", "test was called")
