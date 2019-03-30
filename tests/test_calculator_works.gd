69ec6fe8b03336b03d9a50f84dd043cd6999923d
Darigan <darigan@protonmail.com>
2019-03-30 12:15:39 +0000

    more example images

--- tests/test_calculator_works.gd
+++ tests/test_calculator_works.gd
@@ -5,11 +5,3 @@ func test_calculator_add_works():
 	expect.is_equal(calc.add(5, 5), 10, "5 + 5 is equal to 10")
 	expect.is_equal(calc.subtract(10, 5), 5, "10 - 5 is equal to 5")
 	
-	
-	
-	
-	
-	
-	
-	
-	
