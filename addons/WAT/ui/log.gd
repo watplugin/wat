db81011ba9e257bc4c0727fa8b42035b8b58ebcc
Darigan <code@darigan.ie>
2019-05-11 14:12:13 +0100

    Logger Log's Yield Time Left on Same Line
    Reduces quite a bit of unnecessary lines

--- addons/WAT/ui/log.gd
+++ addons/WAT/ui/log.gd
@@ -3,9 +3,16 @@ tool
 
 var current_line: int = -1
 var timer
+var cache: String
 
 func add_line(msg):
-		self.text += "%s\n" % msg
-		current_line += 1
-		cursor_set_line(current_line)
+		var output = "%s\n" % msg
+		if msg.begins_with("WAT: Yielding"):
+			self.text = cache + output
+		else:
+			cache += output
+			self.text += output
+			current_line += 1
+			cursor_set_line(current_line + 5)
+
 
