104f54b84be3e1b4e4dade7c1ee8a5875d97273b
Darigan <code@darigan.ie>
2019-05-11 16:24:11 +0100

    Add resume method call
    It froze here because we forgot. Need to have it handle itself in the background.

--- tests/folder_a/test_calculator.gd
+++ tests/folder_a/test_calculator.gd
@@ -7,5 +7,5 @@ func test_calculator_add_works():
 	expect.is_equal(calc.add(5, 5), 10, "5 + 5 is equal to 10")
 	expect.is_equal(calc.subtract(10, 5), 5, "10 - 5 is equal to 5")
 	yield(to(self, "a", 10.0), "finished")
-	print("calc finished")
 	expect.is_true(true, "true is true miss calculator")
+	get_parent().resume()
