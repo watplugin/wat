ed401da6062ce82dbde596e4cc072d138ce51666
Darigan <code@darigan.ie>
2019-05-12 14:06:45 +0100

    remove keyword highlighting

--- addons/WAT/ui/log.gd
+++ addons/WAT/ui/log.gd
@@ -5,11 +5,6 @@ var current_line: int = 5
 var timer
 var cache: String
 
-func _init():
-	add_keyword_color("Executing", Color(1, 1, 0, 1))
-	add_keyword_color("Clearing", Color(1, 0, 1, 1))
-	add_keyword_color(".gd", Color(1, 1, 1, 1))
-
 func add_line(msg):
 		var output = "%s\n" % msg
 		if msg.begins_with("Yielding"):
