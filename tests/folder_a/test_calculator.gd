0a21ef3e37d366933bcf9889061e5a87415967cc
Darigan <code@darigan.ie>
2019-05-13 11:52:54 +0100

    Reduced Example Test Yields to 1 Second

--- tests/folder_a/test_calculator.gd
+++ tests/folder_a/test_calculator.gd
@@ -6,6 +6,6 @@ func test_calculator_add_works():
 	var calc = Calculator.new()
 	expect.is_equal(calc.add(5, 5), 10, "5 + 5 is equal to 10")
 	expect.is_equal(calc.subtract(10, 5), 5, "10 - 5 is equal to 5")
-	yield(on_signal(self, "a", 10.0), YIELD)
+	yield(on_signal(self, "a", 1.0), YIELD)
 	expect.is_true(true, "true is true miss calculator")
 #	get_parent().resume()
