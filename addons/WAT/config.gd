e364b9732d1b0dbb2de0c1ce0d54f2beeed8aca3
Darigan <code@darigan.ie>
2019-05-17 14:29:13 +0100

    Expand WATconfig.gd
    No functionality yet. Still require a method to set defaults
    (and a method to accurately fix the UI if we already had wat values
    predefined).

--- addons/WAT/config.gd
+++ addons/WAT/config.gd
@@ -1,10 +1,48 @@
 extends Object
 class_name WATConfig
 
-static func enforce_typed_parameters(checked: CheckBox):
-	print('Setting to %s' % checked.pressed)
-	ProjectSettings.set("wat/parameters", checked.pressed)
+# set double strategy (full, everything gets doubled and doesn't auto-call super OR partial, everything gets doubled and everything calls super default)
+# defaults
+
+static func _set_parameters(tick: CheckBox):
+	ProjectSettings.set("wat/parameters", tick.pressed)
 	
-static func check_enforced_type_parameters() -> bool:
-	var value = ProjectSettings.get("wat/parameters")
-	return value
+static func _set_return_value(tick: CheckBox):
+	ProjectSettings.set("wat/returnvalue", tick.pressed)
+	
+static func _set_exclude_void(tick: CheckBox):
+	ProjectSettings.set("wat/voidexcluded", tick.pressed)
+	
+static func _set_text_prefixes(prefixes: Array):
+	ProjectSettings.set("wat/testprefixes", prefixes)
+	
+static func _set_method_prefixes(prefixes: Array):
+	ProjectSettings.set("wat/methodprefixes", prefixes)
+	
+static func _set_double_strategy(tick: CheckBox):
+	ProjectSettings.set("wat/doublestrategy", tick.pressed)
+	
+static func parameters():
+	return ProjectSettings.get("wat/parameters")
+	
+static func return_value():
+	return ProjectSettings.get("wat/returnvalue")
+	
+static func void_excluded():
+	return ProjectSettings.get("wat/voidexcluded")
+	
+static func test_prefixes():
+	return ProjectSettings.get("wat/testprefixes")
+	
+static func method_prefixes():
+	return ProjectSettings.get("wat/methodprefixes")
+	
+static func double_strategy():
+	return ProjectSettings.get("wat/doublestrategy")
+
+static func defaults():
+	# if not have
+		# create new setting
+	# if setting already exists
+		# affect buttons (ie false = unticked box)
+	pass
