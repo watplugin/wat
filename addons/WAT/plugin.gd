6cfa51298af154c752d2ae1911562ef62a7d75ba
Darigan <code@darigan.ie>
2019-05-19 14:21:40 +0100

    Move WAT main scene to top level

--- addons/WAT/plugin.gd
+++ addons/WAT/plugin.gd
@@ -1,7 +1,7 @@
 tool
 extends EditorPlugin
 
-const UI: PackedScene = preload("UI/WAT.tscn")
+const UI: PackedScene = preload("WAT.tscn")
 var interface: PanelContainer
 
 func _enter_tree() -> void:
