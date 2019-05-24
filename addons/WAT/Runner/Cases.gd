15c4ff21fb6004227fc5375e4b0958d142e6a22e
Darigan <code@darigan.ie>
2019-05-24 13:56:24 +0100

    Refactor CaseManager Node to Cases Script.
    No reason to use it as a node when it is a simple
    data structure that doesn't need tree access.

--- addons/WAT/Runner/Cases.gd
+++ addons/WAT/Runner/Cases.gd
@@ -13,62 +13,62 @@ func create(test) -> void:
 class Case extends Reference:
 	var title: String
 	var methods: Array = []
-	
+
 	func _init(test) -> void:
 		self.title = test.title()
 		test.expect.connect("OUTPUT", self, "_add_expectation")
-		
-	
+
+
 	func add_method(method: String) -> void:
 		methods.append(Method.new(method))
-	
+
 	func _add_expectation(success: bool, expected: String, result: String, notes: String) -> void:
 		methods.back().expectations.append(Expectation.new(success, expected, result, notes))
-		
+
 	func total() -> int:
 		return methods.size()
-		
+
 	func successes() -> int:
 		var success: int = 0
 		for method in methods:
 			if method.success():
 				success += 1
 		return success
-		
+
 	func success() -> bool:
 		if total() <= 0:
 			return false
 		return successes() == total()
-			
+
 
 class Method extends Reference:
 	var title: String
 	var expectations: Array = []
-	
+
 	func _init(title) -> void:
 		self.title = title.substr(title.find("_"), title.length()).replace("_", " ").dedent()
-	
+
 	func total() -> int:
 		return expectations.size()
-		
+
 	func successes() -> int:
 		var success: int = 0
 		for expectation in expectations:
 			if expectation.success:
 				success += 1
 		return success
-		
+
 	func success() -> bool:
 		if total() <= 0:
 			return false
 		return successes() == total()
-	
+
 class Expectation extends Reference:
 	var success: bool
 	var expected: String
 	var result: String
 	var notes: String
-	
+
 	func _init(success, expected, result, notes):
 		self.success = success
 		self.expected = expected
--- addons/WAT/Runner/runner.gd
+++ addons/WAT/Runner/runner.gd
@@ -4,7 +4,7 @@ tool
 const TEST = preload("res://addons/WAT/test/test.gd")
 const IO = preload("res://addons/WAT/utils/input_output.gd")
 onready var Yield = $Yielder
-onready var CaseManager = $CaseManager
+onready var Cases = load("res://addons/WAT/Runner/Cases.gd").new()
 onready var Collect = $Collect
 signal display_results
 signal output
@@ -50,7 +50,7 @@ func _loop() -> void:
 	
 func start() -> void:
 	test = tests.pop_front().new()
-	CaseManager.create(test)
+	Cases.create(test)
 	add_child(test)
 	methods = Collect.methods(test)
 	test.start()
@@ -61,7 +61,7 @@ func execute() -> void:
 		var method: String = methods.pop_front()
 		var clean = method.substr(method.find("_"), method.length()).replace("_", " ").dedent()
 		output("Executing Method: %s" % clean)
-		CaseManager.current.add_method(method)
+		Cases.current.add_method(method)
 		test.pre()
 		test.call(method)
 		if yielding():
@@ -71,7 +71,7 @@ func execute() -> void:
 	end()
 		
 func log_method():
-	var method = CaseManager.current.methods.back()
+	var method = Cases.current.methods.back()
 	for expect in method.expectations:
 		var details = expect.expected.lstrip("Expect:").dedent()
 		var msg = "%s:  %s" % ["PASSED" if expect.success else "FAILED", details]
@@ -80,7 +80,7 @@ func log_method():
 	output(msg)
 	
 func log_test():
-	var case = CaseManager.current
+	var case = Cases.current
 	var msg = "%s:  %s" % ["PASSED" if case.success() else "FAILED", case.title]
 	output(msg)
 		
@@ -94,12 +94,12 @@ func end() -> void:
 	IO.clear_all_temp_directories()
 	
 func _finish() -> void:
-	emit_signal("display_results", CaseManager.list)
+	emit_signal("display_results", Cases.list)
 	
 func clear() -> void:
 	tests.clear()
 	methods.clear()
-	CaseManager.list.clear()
+	Cases.list.clear()
 	
 func resume() -> void:
 	test.post()
