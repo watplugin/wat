a000bd1a2daf53c3021e8e2d33404b9c506bce18
Darigan <code@darigan.ie>
2019-06-18 14:57:09 +0100

    Add Script of Consts to Expectation Methods.
    This was mainly to make bootstrap tests easier on
    me later.

--- addons/WAT/expectations/0_index.gd
+++ addons/WAT/expectations/0_index.gd
@@ -1,5 +1,4 @@
-extends Reference
-
+const EXPECT = preload("res://addons/WAT/constants/expectation_list.gd")
 signal OUTPUT
 
 func output(data) -> void:
@@ -7,106 +6,106 @@ func output(data) -> void:
 	emit_signal("OUTPUT", data)
 
 func is_true(condition: bool, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_true.gd").new(condition, expected))
+	output(EXPECT.IS_TRUE.new(condition, expected))
 
 func is_false(condition: bool, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_false.gd").new(condition, expected))
+	output(EXPECT.IS_FALSE.new(condition, expected))
 
 func is_equal(a, b, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_equal.gd").new(a, b, expected))
+	output(EXPECT.IS_EQUAL.new(a, b, expected))
 
 func is_not_equal(a, b, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_not_equal.gd").new(a, b, expected))
+	output(EXPECT.IS_NOT_EQUAL.new(a, b, expected))
 
 func is_greater_than(a, b, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_greater_than.gd").new(a, b, expected))
+	output(EXPECT.IS_GREATER_THAN.new(a, b, expected))
 
 func is_less_than(a, b, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_less_than.gd").new(a, b, expected))
+	output(EXPECT.IS_LESS_THAN.new(a, b, expected))
 
 func is_equal_or_greater_than(a, b, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_equal_or_greater_than.gd").new(a, b, expected))
+	output(EXPECT.IS_EQUAL_OR_GREATER_THAN.new(a, b, expected))
 
 func is_equal_or_less_than(a, b, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_equal_or_less_than.gd").new(a, b, expected))
+	output(EXPECT.IS_EQUAL_OR_LESS_THAN.new(a, b, expected))
 
 func is_in_range(value, low, high, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_in_range.gd").new(value, low, high, expected))
+	output(EXPECT.IS_IN_RANGE.new(value, low, high, expected))
 
 func is_not_in_range(value, low, high, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_not_in_range.gd").new(value, low, high, expected))
+	output(EXPECT.IS_NOT_IN_RANGE.new(value, low, high, expected))
 
 func has(value, container, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/has.gd").new(value, container, expected))
+	output(EXPECT.HAS.new(value, container, expected))
 
 func does_not_have(value, container, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/does_not_have.gd").new(value, container, expected))
+	output(EXPECT.DOES_NOT_HAVE.new(value, container, expected))
 
 func is_class_instance(instance, type, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_class_instance.gd").new(instance, type, expected))
+	output(EXPECT.IS_CLASS_INSTANCE.new(instance, type, expected))
 
 func is_not_class_instance(instance, type, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_not_class_instance.gd").new(instance, type, expected))
+	output(EXPECT.IS_NOT_CLASS_INSTANCE.new(instance, type, expected))
 
 func is_built_in_type(value, type, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_built_in_type.gd").new(value, type, expected))
+	output(EXPECT.IS_BUILT_IN_TYPE.new(value, type, expected))
 
 func is_not_built_in_type(value, type: int, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_not_built_in_type.gd").new(value, type, expected))
+	output(EXPECT.IS_NOT_BUILT_IN_TYPE.new(value, type, expected))
 
 func is_null(value, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_null.gd").new(value, expected))
+	output(EXPECT.IS_NULL.new(value, expected))
 
 func is_not_null(value, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/is_not_null.gd").new(value, expected))
+	output(EXPECT.IS_NOT_NULL.new(value, expected))
 
 func string_contains(value, string: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/string_contains.gd").new(value, string, expected))
+	output(EXPECT.STRING_CONTAINS.new(value, string, expected))
 
 func string_does_not_contain(value, string: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/string_does_not_contain.gd").new(value, string, expected))
+	output(EXPECT.STRING_DOES_NOT_CONTAIN.new(value, string, expected))
 
 func string_begins_with(value, string: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/string_begins_with.gd").new(value, string, expected))
+	output(EXPECT.STRING_BEGINS_WITH.new(value, string, expected))
 
 func string_does_not_begin_with(value, string: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/string_does_not_begin_with.gd").new(value, string, expected))
+	output(EXPECT.STRING_DOES_NOT_BEGIN_WITH.new(value, string, expected))
 
 func string_ends_with(value, string: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/string_ends_with.gd").new(value, string, expected))
+	output(EXPECT.STRING_ENDS_WITH.new(value, string, expected))
 
 func string_does_not_end_with(value, string: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/string_does_not_end_with.gd").new(value, string, expected))
+	output(EXPECT.STRING_DOES_NOT_END_WITH.new(value, string, expected))
 
 func was_called(double, a: String = "", b: String = "", c: String = "") -> void:
 	_scene_was_called(double, a, b, c) if double.is_scene else _script_was_called(double, a, b)
 
 func _scene_was_called(double, nodepath: String, method: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/scene_was_called.gd").new(double, nodepath, method, expected))
+	output(EXPECT.SCENE_WAS_CALLED.new(double, nodepath, method, expected))
 
 func _script_was_called(double, method: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/script_was_called.gd").new(double, method, expected))
+	output(EXPECT.SCRIPT_WAS_CALLED.new(double, method, expected))
 
 func was_not_called(double, a: String = "", b: String = "", c: String = "") -> void:\
 	_scene_was_not_called(double, a, b, c) if double.is_scene else _script_was_not_called(double, a, b)
 #
 func _scene_was_not_called(double, nodepath: String, method: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/scene_was_not_called.gd").new(double, nodepath, method, expected))
+	output(EXPECT.SCENE_WAS_NOT_CALLED.new(double, nodepath, method, expected))
 #
 func _script_was_not_called(double, method: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/script_was_not_called.gd").new(double, method, expected))
+	output(EXPECT.SCRIPT_WAS_NOT_CALLED.new(double, method, expected))
 
 func was_called_with_arguments(double, method: String, arguments: Dictionary, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/called_with_arguments.gd").new(double, method, arguments, expected))
+	output(EXPECT.CALLED_WITH_ARGUMENTS.new(double, method, arguments, expected))
 
 func signal_was_emitted(emitter, _signal, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/signal_was_emitted.gd").new(emitter, _signal, expected))
+	output(EXPECT.SIGNAL_WAS_EMITTED.new(emitter, _signal, expected))
 
 func signal_was_not_emitted(emitter, _signal: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/signal_was_not_emitted.gd").new(emitter, _signal, expected))
+	output(EXPECT.SIGNAL_WAS_NOT_EMITTED.new(emitter, _signal, expected))
 	
 func file_exists(path: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/file_exists.gd").new(path, expected))
+	output(EXPECT.FILE_EXISTS.new(path, expected))
 	
 func file_does_not_exist(path: String, expected: String) -> void:
-	output(load("res://addons/WAT/expectations/file_does_not_exist.gd").new(path, expected))
+	output(EXPECT.FILE_DOES_NOT_EXIST.new(path, expected))
