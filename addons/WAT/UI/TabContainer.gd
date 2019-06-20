1f37d26ab6eba6123070a436090f4586757a65d3
CodeDarigan <code@darigan.ie>
2019-06-20 18:41:49 +0100

    Remove old files

--- addons/WAT/UI/TabContainer.gd
+++
@@ -1,5 +0,0 @@
-extends TabContainer
-tool
-#
-#func _ready():
-#	$Runner/Layout.rect_size = self.rect_size
--- addons/WAT/UI/log_level.gd
+++
@@ -1,6 +0,0 @@
-extends OptionButton
-
-func _enter_tree():
-	add_item("Only Failed", 0)
-	add_item("Passed/Failed", 1)
-	add_item("All", 2)
