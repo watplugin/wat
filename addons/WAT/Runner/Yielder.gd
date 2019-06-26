4807e156af536987c97c32d30c726279cfd8d210
Darigan <code@darigan.ie>
2019-06-26 18:05:47 +0100

    Refactor Yielder Code

--- addons/WAT/Runner/Yielder.gd
+++ addons/WAT/Runner/Yielder.gd
@@ -7,6 +7,9 @@ signal resume
 func until_signal(time_limit: float, emitter: Object, event: String) -> YieldTimer:
 	output("Yielding: { signal: %s, emitter: %s, time: %s }" % [event, emitter, time_limit])
 	var yield_timer: YieldTimer = YieldTimer.new(time_limit, emitter, event)
+	return start(yield_timer)
+	
+func start(yield_timer):
 	queue.append(yield_timer)
 	add_child(yield_timer)
 	yield_timer.start()
@@ -15,10 +18,7 @@ func until_signal(time_limit: float, emitter: Object, event: String) -> YieldTim
 func until_timeout(time_limit: float) -> YieldTimer:
 	output("Yielding: { time: %s }" % time_limit)
 	var yield_timer: YieldTimer = YieldTimer.new(time_limit, self, "", true)
-	queue.append(yield_timer)
-	add_child(yield_timer)
-	yield_timer.start()
-	return yield_timer
+	return start(yield_timer)
 
 func resume(yield_timer: YieldTimer):
 	remove_child(yield_timer)
@@ -35,29 +35,21 @@ class YieldTimer extends Timer:
 	signal finished
 	var emitter: Object
 	var event: String
-	var resumed: bool = false
 
 	func _init(time_limit: float, emitter: Object, event: String, time_limit_only: bool = false) -> void:
 		one_shot = true
 		wait_time = time_limit
-		connect("timeout", self, "_on_timeout")
+		connect("timeout", self, "_resume")
 		if time_limit_only:
 			return
 		self.emitter = emitter
 		self.event = event
-		emitter.connect(event, self, "_on_signal")
+		emitter.connect(event, self, "_resume")
 
-	func _on_timeout():
+	func _resume(a = null, b = null, c = null, d = null, e = null, f = null, g = null, h = null, i = null, j = null, k = null):
 		emit_signal("finished")
-		if not resumed:
-			resumed = true
-			get_parent().resume(self)
-
-	func _on_signal(a = null, b = null, c = null, d = null, e = null, f = null, g = null, h = null, i = null, j = null, k = null):
-		emit_signal("finished")
-		if not resumed:
-			resumed = true
-			get_parent().resume(self)
+		set_block_signals(true)
+		get_parent().resume(self)
 
 	func _process(delta):
 		if emitter != null and event != null:
