4057b4adb9b7bceb61734257f4d43c07ccd469d1
CodeDarigan <code@darigan.ie>
2019-06-27 03:14:33 +0100

    Refactor of Yielder complete
    Long methods but mainly construction, so no major issue.

--- addons/WAT/Runner/Yielder.gd
+++ addons/WAT/Runner/Yielder.gd
@@ -3,41 +3,39 @@ tool
 
 signal resume
 var queue: Array
+var message: String
 
 func until_signal(time_limit: float, emitter: Object, event: String) -> Timer:
-	output("Yielding: { signal: %s, emitter: %s, time: %s }" % [event, emitter, time_limit])
-	var yield_timer: Timer = Timer.new()
-	yield_timer.wait_time = time_limit
-	emitter.connect(event, yield_timer, "emit_signal", ["finished"])
-	return start(yield_timer)
-
-func start(yield_timer):
-	yield_timer.add_user_signal("finished")
-	yield_timer.one_shot = true
-	yield_timer.connect("timeout", yield_timer, "emit_signal", ["finished"])
-	yield_timer.connect("finished", self, "resume", [yield_timer], CONNECT_DEFERRED)
-	queue.append(yield_timer)
-	add_child(yield_timer)
-	yield_timer.start()
-	return yield_timer
+	var timer: Timer = _setup_timer(time_limit)
+	message = "Yielding: { signal: %s, emitter: %s, time: {timeleft}}" % [event, emitter]
+	emitter.connect(event, timer, "emit_signal", ["finished"])
+	timer.start()
+	return timer
 
 func until_timeout(time_limit: float) -> Timer:
-	output("Yielding: { time: %s }" % time_limit)
-	var yield_timer: Timer = Timer.new()
-	yield_timer.wait_time = time_limit
-	return start(yield_timer)
+	var timer = _setup_timer(time_limit)
+	message = "Yielding: { time: {timeleft}}"
+	timer.start()
+	return timer
 
-func resume(yield_timer: Timer):
-	print("resume called")
-	queue.erase(yield_timer)
-	yield_timer.queue_free()
+func _setup_timer(timer_limit: float) -> Timer:
+	var timer: Timer = Timer.new()
+	timer.wait_time =timer_limit
+	timer.one_shot = true
+	timer.add_user_signal("finished")
+	timer.connect("timeout", timer, "emit_signal", ["finished"])
+	timer.connect("finished", self, "_resume", [timer], CONNECT_DEFERRED)
+	queue.append(timer)
+	add_child(timer)
+	return timer
+
+func _resume(timer: Timer) -> void:
+	queue.erase(timer)
+	timer.queue_free()
 	if queue.size() > 0:
 		return
 	emit_signal("resume") # This resume is for the runner
 
-func output(msg):
-	get_parent().output(msg)
-
 func _process(delta):
 	if queue.size() > 0:
-		get_parent().output(str(queue.back().time_left))
+		get_parent().output(message.format({"timeleft": queue.back().time_left}))
