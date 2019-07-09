ca3262acc3be8b4d574b3ff331eb4228aad154c6
CodeDarigan <code@darigan.ie>
2019-07-09 22:05:24 +0100

    Add Expect String to IsNotEqual Expectation Object

--- addons/WAT/expectations/is_not_equal.gd
+++ addons/WAT/expectations/is_not_equal.gd
@@ -4,4 +4,5 @@ extends "base.gd"
 func _init(a, b, context: String) -> void:
 	self.success = (a != b)
 	self.context = "[Expect.IsNotEqual] %s" % context
+	self.expected = "|%s| %s != |%s| %s" % [type2str(a), a, type2str(b), b]
 	self.result = "|%s| %s %s |%s| %s" % [type2str(a), a, ("!=" if self.success else "=="), type2str(b),b]
