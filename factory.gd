e7423b5126fa3360f1f969592f6667739c1b3ee4
CodeDarigan <code@darigan.ie>
2019-07-13 13:34:07 +0100

    Remove loops in favor of expression object

--- factory.gd
+++ factory.gd
@@ -12,18 +12,10 @@ var cache: Array = []
 var count: int = 0
 
 func double(path, inner: String, dependecies: Array, container: Reference, use_container: bool):
-	var doubler = Doubler.new()
-	var index = FILESYSTEM.file_list("user://WATemp").size() as String
-	index += count as String
-	count += 1
-	var savepath: String = "user://WATemp/R%s.tres" % index as String
-	doubler.base_script = path
-	doubler.inner = inner
-	doubler.index = index
-	ResourceSaver.save(savepath, doubler)
-	var double = load(savepath)
+	var save_path: String = save_double(path, inner, dependecies)
+	var double = load(save_path)
+
 	var base
-	double.dependecies = dependecies
 	if use_container:
 		base = load_using_container(path, inner, container, double)
 	elif not use_container:
@@ -32,15 +24,28 @@ func double(path, inner: String, dependecies: Array, container: Reference, use_c
 		double.base_methods[m.name] = "a,b,c,d,e,f,g,h,i,j,".substr(0, m.args.size() * 2 - 1)
 	return double
 
+func save_double(path, inner, dependecies) -> String:
+	var doubler = Doubler.new()
+	var index = FILESYSTEM.file_list("user://WATemp").size() as String
+	index += count as String
+	count += 1
+	var savepath: String = "user://WATemp/R%s.tres" % index as String
+	doubler.base_script = path
+	doubler.inner = inner
+	doubler.index = index
+	doubler.dependecies = dependecies
+	ResourceSaver.save(savepath, doubler)
+	return savepath
+
 func load_using_container(path, inner, container, double):
 	var base = container.resolve(load(path)) # We're doubling an inner so this doesn't exist?
 	double.dependecies = container.get_constructor(load(path))
 	cache.append(base)
 	if inner != "":
-		for i in inner.split(".", false):
-			double.dependecies = container.get_constructor(base.get(i))
-			base = container.resolve(base.get(i))
-			cache.append(base)
+		base = load_inner(path, inner, container)
+		double.dependecies = container.get_constructor(base)
+		base = container.resolve(base)
+		cache.append(base)
 	return base
 
 func load_not_using_container(path, inner):
@@ -48,13 +53,13 @@ func load_not_using_container(path, inner):
 	cache.append(base)
 	if inner != "":
 		print(inner, "x")
-		return load_inner(path, inner)
+		return load_inner(path, inner).new()
 	return base
 
-func load_inner(path, inner_classes) -> Object:
+func load_inner(path, inner_classes, container = null) -> Object:
 	var expression = Expression.new()
 	var script = load(path)
-	expression.parse("%s.new()" % [inner_classes])
+	expression.parse("%s" % [inner_classes])
 	return expression.execute([], script, true)
 
 func clear():
