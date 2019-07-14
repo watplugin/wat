7de66707c78e69e75b558f62f12912ae901d1e43
CodeDarigan <code@darigan.ie>
2019-07-14 21:23:22 +0100

    Fix Bug Crash
    Kept trying to look for something that didn't exist.

--- addons/WAT/Runner/runner.gd
+++ addons/WAT/Runner/runner.gd
@@ -10,6 +10,7 @@ var filesystem: Reference
 var validate: Reference
 var tests: Array = []
 var caselist: Array = []
+signal errored
 signal ended
 
 func _init(validate: Reference, filesystem: Reference, settings: Resource, Results) -> void:
@@ -18,7 +19,11 @@ func _init(validate: Reference, filesystem: Reference, settings: Resource, Resul
 	self.settings = settings
 	self.Results = Results
 
-func _run(directory: String = "res://tests") -> void:
+func _run(directory: String = "res://testSSSs") -> void:
+	if not Directory.new().dir_exists(directory):
+		emit_signal("errored")
+		push_error("WAT: Test Directory: %s does not exist" % directory)
+		return
 	clear()
 	print("WAT: Starting Test Runner")
 	self.tests = validate.tests(filesystem.file_list(directory), settings.test_script_prefixes)
