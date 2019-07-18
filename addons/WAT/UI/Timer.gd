d4d7c81f6ed7c9d5db3930b5d7998c098a97b6c1
CodeDarigan <code@darigan.ie>
2019-07-18 21:27:56 +0100

    Remove debug prints

--- addons/WAT/UI/Timer.gd
+++ addons/WAT/UI/Timer.gd
@@ -5,14 +5,12 @@ var running = false
 var base: float
 
 func _ready():
-	print("running timer")
 	self.text = "0"
 	set_process(true)
 
 func _process(delta):
 	if running:
 		self.text = str(OS.get_ticks_msec() - base) + " ms"
-		self.text += "1"
 
 func _start() -> void:
 	self.text = "0"
@@ -20,6 +18,5 @@ func _start() -> void:
 	running = true
 
 func _stop() -> void:
-	print("stopping timer")
 	running = false
 	self.text = str(OS.get_ticks_msec() - base) + " ms"
