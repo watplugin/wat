4b935cd3a313fda96e99110bdf40d915d15f23ba
CodeDarigan <code@darigan.ie>
2019-07-19 14:27:32 +0100

    Inline methods
    Didn't seem worth creating a variable specifically for them.

--- addons/WAT/WAT.gd
+++ addons/WAT/WAT.gd
@@ -13,13 +13,11 @@ func _ready():
 	Results.settings = SETTINGS
 	Runner = RUNNER.new(VALIDATE, FILESYSTEM, SETTINGS, Results)
 	add_child(Runner)
+	get_node("Runner/Options").connect("RUN", self, "_run")
 
-	# Runner is a misnomer?
-	var RunAll = get_node("Runner/Options/VBox/RunAll")
+	# Need to refactor this to be a lot less odd?
 	var Expand = get_node("Runner/Options/VBox/Expand")
-	var Options = get_node("Runner/Options")
 	Expand.connect("pressed", Results, "_collapse_all", [Expand])
-	Options.connect("RUN", self, "_run")
 
 func _run(path: String = "res://tests") -> void:
 	Runner.run(path) # Our call-deferred may cause issues here
