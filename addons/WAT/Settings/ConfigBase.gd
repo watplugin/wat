e5a979b0d65a84da3ce95722782295ba1d16552a
CodeDarigan <code@darigan.ie>
2019-07-19 15:06:31 +0100

    Remove double related settings.
    Doubles aren't the System under test, so we try to
    be as mallable as possible with them.

--- addons/WAT/Settings/ConfigBase.gd
+++ addons/WAT/Settings/ConfigBase.gd
@@ -2,17 +2,6 @@ extends Resource
 
 # Use The Settings in WAT instead
 
-enum S {
-	DOUBLE
-	PARTIAL
-}
-
-### BEGIN DELETE
-export(bool) var keep_typed_parameters_in_doubled_scripts = true
-export(bool) var keep_typed_return_values_in_doubled_scripts = true
-export(bool) var exclude_void_typed_return_values_in_doubled_scripts = true
-export(bool) var double_strategy_is_partial = false
-### END DELETE
 
 ### BEGIN GUI ONLY
 export(bool) var tests_include_subdirectories = true
