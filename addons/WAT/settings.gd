84de80fe3280475286985ce36654fbb307283ece
CodeDarigan <code@darigan.ie>
2019-07-20 20:25:02 +0100

    Remove a "test subdir" setting
    We have the ability to test single folders so that should suffice instead.

--- addons/WAT/settings.gd
+++ addons/WAT/settings.gd
@@ -2,7 +2,6 @@ extends Reference
 
 static func defaults():
 	_test_directory()
-	_using_subdirectories()
 	_display_directories_in_their_own_tabs()
 
 static func _test_directory() -> void:
@@ -12,15 +11,6 @@ static func _test_directory() -> void:
 	push_warning("You have not set a test directory. You can set it in Project -> Project Settings -> General -> WAT")
 	ProjectSettings.set(title, "")
 	var property_info: Dictionary = {"name": title, "type": TYPE_STRING, "hint_string": "Store your WATTests here"}
-	ProjectSettings.add_property_info(property_info)
-
-static func _using_subdirectories() -> void:
-	var title: String = "WAT/Include_subdirectories_when_running_tests"
-	if ProjectSettings.has_setting(title):
-		return
-	ProjectSettings.set(title, true)
-	var hint_string: String = "All tests in subdirectories will be run (this includes subdirectories within subdirectores as well)"
-	var property_info: Dictionary = {"name": title, "type": TYPE_BOOL, "hint_string": hint_string}
 	ProjectSettings.add_property_info(property_info)
 
 static func _display_directories_in_their_own_tabs() -> void:
