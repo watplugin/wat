9bc2a97076f256ceb8e3838a24b799d499049fe3
CodeDarigan <code@darigan.ie>
2019-07-20 20:40:19 +0100

    Housekeeping

--- addons/WAT/UI/results.gd
+++ addons/WAT/UI/results.gd
@@ -6,9 +6,6 @@ var directories: Dictionary = {}
 var tab: int = 0
 
 func display(cases: Array) -> void:
-#	_display_directories_as_individual_tabs(cases)
-#
-#func _display_directories_as_individual_tabs(cases: Array) -> void:
 	_add_directories(cases)
 	for directory in directories:
 		_add_result_display(directories[directory], directory)
