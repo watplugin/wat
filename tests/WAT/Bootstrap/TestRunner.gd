7601c06de00d272d30df0424d971544e42008541
CodeDarigan <code@darigan.ie>
2019-07-22 16:29:40 +0100

    Add pre and post condition methods to TestRunner
    Removes quite a lot of boilerplate alright.

--- tests/WAT/Bootstrap/TestRunner.gd
+++ tests/WAT/Bootstrap/TestRunner.gd
@@ -1,17 +1,27 @@
 extends WATTest
 
 var cases: Array = []
+var filesystem
+var runner
 
 func title():
 	return "TestRunner"
 	
-func test_Runner_with_no_tests():
-	describe("Running no tests")
-
-	var filesystem = load("res://addons/WAT/filesystem.gd")
-	var runner: Node = load("res://addons/WAT/Runner/runner.gd").new(filesystem)
+func pre():
+	cases = []
+	filesystem = load("res://addons/WAT/filesystem.gd")
+	runner = load("res://addons/WAT/Runner/runner.gd").new(filesystem)
 	runner.connect("ended", self, "display")
 	add_child(runner)
+
+func post():
+	filesystem = null
+	remove_child(runner)
+	runner.disconnect("ended", self, "display")
+	runner.queue_free()
+	
+func test_Runner_with_no_tests():
+	describe("Running no tests")
 
 	# Act
 	runner.run("res://Examples/Bootstrap/Empty")
@@ -20,20 +30,8 @@ func test_Runner_with_no_tests():
 	# Assert
 	expect.is_true(cases.empty(), "Runner emits no testcases (times out at 2 seconds)")
 
-	# Teardown
-	cases = []
-	remove_child(runner)
-	runner.queue_free()
-	
-
 func test_Runner_with_one_passing_test():
 	describe("Running 1 correct test")
-	
-	# Arrange
-	var filesystem = load("res://addons/WAT/filesystem.gd")
-	var runner: Node = load("res://addons/WAT/Runner/runner.gd").new(filesystem)
-	runner.connect("ended", self, "display")
-	add_child(runner)
 	
 	# Guard (If this fails, then we have left over data from last test)
 	expect.is_true(cases.empty(), "Before running, testcases are empty")
@@ -45,11 +43,6 @@ func test_Runner_with_one_passing_test():
 	# Assert
 	expect.is_equal(cases.size(), 1, "emits a list of 1 testcase")
 	expect.is_true(cases[0].success, "which is a success")
-	
-	# Teardown
-	cases = []
-	remove_child(runner)
-	runner.queue_free()
 	
 func display(cases):
 	# HelperMethod
