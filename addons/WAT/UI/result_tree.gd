578da926a2669e84366184f40a8f7a94a4a46818
CodeDarigan <code@darigan.ie>
2019-07-22 16:00:34 +0100

    Clean merge cruft

--- addons/WAT/UI/result_tree.gd
+++ addons/WAT/UI/result_tree.gd
@@ -57,14 +57,11 @@ func _add_script_data(script: TreeItem, data) -> void:
 	cache.append(script)
 
 func _add_method_data(method: TreeItem, data: Dictionary) -> void:
-<<<<<<< HEAD
 	method.set_text(0, "%s" % data.context)
-	method.set_icon(0, ICON_SUCCESS if data.success else ICON_FAILED)
-=======
+	method.set_icon(0, success() if data.success else failed())
 	method.collapsed = true
 	method.set_text(0, "(%s/%s) %s" % [data.passed, data.total, data.context])
 	method.set_icon(0, success() if data.success else failed())
->>>>>>> master
 	method.set_custom_color(0, COLOR_SUCCESS if data.success else COLOR_FAILED)
 	method.set_tooltip(0, "Source: %s" % data.title)
 
