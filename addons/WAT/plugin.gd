f22059a58153770b4e8c18cd1dab813d6b025d81
CodeDarigan <code@darigan.ie>
2019-07-23 12:34:04 +0100

    Move Settings to Plugin.gd
    One method isn't enough for a script really.

--- addons/WAT/plugin.gd
+++ addons/WAT/plugin.gd
@@ -2,15 +2,15 @@ tool
 extends EditorPlugin
 
 const UI: PackedScene = preload("WAT.tscn")
-const SETTINGS: Reference = preload("res://addons/WAT/settings.gd")
 var interface: PanelContainer
 
 func _enter_tree() -> void:
-	_create_wat_temp_folder()
+	_create_test_folder()
+	_create_temp_folder()
 	interface = UI.instance()
 	get_editor_interface().get_editor_viewport().add_child(interface)
 	make_visible(false)
-	SETTINGS.defaults()
+
 
 
 func _exit_tree() -> void:
@@ -26,5 +26,14 @@ func make_visible(visible: bool) -> void:
 func get_plugin_name() -> String:
    return "WAT"
 
-func _create_wat_temp_folder() -> void:
+func _create_temp_folder() -> void:
 	Directory.new().make_dir("user://WATemp")
+	
+func _create_test_folder() -> void:
+	var title: String = "WAT/Test_Directory"
+	if not ProjectSettings.has_setting(title):
+		var property_info: Dictionary = {"name": title, "type": TYPE_STRING, "hint_string": "Store your WATTests here"}
+		ProjectSettings.set(title, "res://tests")
+		ProjectSettings.add_property_info(property_info)
+		push_warning("Set Test Directory to 'res://'. You can change this in Project -> Project Settings -> General -> WAT")
+		return
