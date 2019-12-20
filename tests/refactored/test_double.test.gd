e9bc8a867bbd7925272c1e067db65de84d357e8b
CodeDarigan <code@darigan.ie>
2019-12-20 17:13:12 +0000

    Refactor More Tests (Move Some Olds Ones Too)
    Gotta say these are looking a lot nicer and
    straightforward.

--- tests/refactored/test_double.test.gd
+++ tests/refactored/test_double.test.gd
@@ -1,19 +1,22 @@
 extends WAT.Test
 
+var director
+
 func title():
 	# Script Directors are responsible for setting up Test Double Scripts
 	return "Given a Test Double"
+	
+func pre():
+	director = direct.script("res://Examples/Scripts/calculator.gd")
 
 func test_When_we_call_an_add_method_that_we_have_not_directed():
 	describe("When we call an add(x, y) method that we haven't directed")
 	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	asserts.is_equal(4, director.double().add(2, 2), "Then we get the correct result")
 
 func test_When_we_call_a_method_that_we_have_dummied():
 	describe("When we call a method that we have dummied")
 	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	director.method("add").dummy()
 	
 	asserts.is_null(director.double().add(2, 2), "Then we get null")
@@ -21,7 +24,6 @@ func test_When_we_call_a_method_that_we_have_dummied():
 func test_When_we_call_a_method_that_we_have_stubbed_to_return_true():
 	describe("When we call a method that we have stubbed to return true")
 	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	director.method("add").stub(true)
 	
 	asserts.is_true(director.double().add(2, 2), "Then it returns true")
@@ -29,7 +31,6 @@ func test_When_we_call_a_method_that_we_have_stubbed_to_return_true():
 func test_When_we_call_a_method_that_we_have_stubbed_to_return_a_node():
 	describe("When we call a method that we have stubbed to return a node")
 	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	var node: Node = Node.new()
 	director.method("add").stub(node)
 	
@@ -40,7 +41,6 @@ func test_When_we_call_a_method_that_we_are_spying_on():
 	# Add a not-called version?
 	describe("When we call a method that we are spying on")
 	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	director.method("add").spy()
 	director.double().add(2, 2)
 	
@@ -49,7 +49,6 @@ func test_When_we_call_a_method_that_we_are_spying_on():
 func test_When_we_pass_arguments_to_a_method_call_that_we_are_spying_on():
 	describe("When we pass arguments to a method call that we are spying on")
 	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	director.method("add").spy()
 	director.double().add(10, 10)
 	
@@ -57,8 +56,6 @@ func test_When_we_pass_arguments_to_a_method_call_that_we_are_spying_on():
 
 func test_When_we_call_a_method_that_was_stubbed_to_return_different_values_based_on_argument_patterns():
 	describe("When we call a method that was stubbed to return different values based on argument patterns")
-	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	
 	director.method("add").stub(100).stub(1000, [1, 1])
 	var double = director.double()
@@ -70,7 +67,6 @@ func test_When_we_call_a_method_that_was_stubbed_with_an_argument_pattern_that_i
 	describe("When we call a method that was stubbed with an argument pattern that includes a non-primitive object")
 	
 	var non_primitive_object: Node = Node.new()
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	director.method("add").stub(9999, [0, non_primitive_object])
 	
 	asserts.is_equal(9999, director.double().add(0, non_primitive_object), "Then it returns the corresponding value when the pattern matches")
@@ -78,7 +74,6 @@ func test_When_we_call_a_method_that_was_stubbed_with_an_argument_pattern_that_i
 func test_When_we_call_a_method_that_was_stubbed_with_a_partial_argument_pattern():
 	describe("When we call a method that was stubbed with a partial (ie using any()) argument pattern")
 	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	director.method("add").stub(9999, [10, any()])
 	
 	asserts.is_equal(9999, director.double().add(10, 42), "Then it returns the corresponding value when the partial pattern matches")
@@ -86,7 +81,6 @@ func test_When_we_call_a_method_that_was_stubbed_with_a_partial_argument_pattern
 func test_When_we_call_a_method_that_we_stubbed_to_call_its_super_implementation_by_default():
 	describe("When we call a method that we stubbed to call its super implementation by default")
 	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	director.method("add").call_super()
 	director.method("add").stub(9999, [10, 10])
 	var double = director.double()
@@ -98,16 +92,14 @@ func test_When_we_call_a_method_that_we_stubbed_to_call_its_super_implementation
 func test_When_we_add_a_doubled_inner_class_to_it():
 	describe("When we add an doubled inner class to it")
 
-	var primary = direct.script("res://Examples/Scripts/calculator.gd")
 	var inner = direct.script("res://Examples/Scripts/calculator.gd", "Algebra")
-	primary.add_inner_class(inner, "Algebra")
+	director.add_inner_class(inner, "Algebra")
 	
-	asserts.is_equal(TAU, primary.double().Algebra.get_tau(), "Then we can call the static methods of that inner double")
+	asserts.is_equal(TAU, director.double().Algebra.get_tau(), "Then we can call the static methods of that inner double")
 	
 func test_When_we_stubbed_a_keyword_method_by_passing_in_the_correct_keyword():
 	describe("When we stubbed a keyworded method by passing in the correct keyword")
 	
-	var director = direct.script("res://Examples/Scripts/calculator.gd")
 	director.method("pi", director.STATIC).stub(true, [])
 	
 	asserts.is_equal(director.double().pi(), true, "Then we can call it")
