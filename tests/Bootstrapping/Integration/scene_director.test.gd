e4d98d2327931a8855fe87ba1a970be85d44d124
CodeDarigan <code@darigan.ie>
2019-12-24 01:39:39 +0000

    Comment Out Methods

--- tests/Bootstrapping/Integration/scene_director.test.gd
+++ tests/Bootstrapping/Integration/scene_director.test.gd
@@ -28,27 +28,27 @@ func test_When_we_create_two_of_it_for_the_same_scene():
 	
 	asserts.is_not_equal(director.nodes, director_b.nodes, "Then they do not share resources")
 	
-func test_When_we_call_a_method_from_the_root_node_that_we_stubbed():
-	describe("When we call a method from the root node that we stubbed")
-	
-	print("Testing When we call a method from the root node that we stubbed")
-	director.get_node(".").method("test").stub(9999)
-	
-	asserts.is_equal(9999, director.double().get_node(".").test(), "Then we get the stubbed return value")
-	
-func test_When_we_call_a_method_from_a_child_node_that_we_stubbed():
-	describe("When we call a method from a child node that we stubbed")
-	
-	print("Testing When we call a method from a chidl node that we stubbed")
-	director.get_node("A").method("execute").stub(999)
-	
-	asserts.is_equal(999, director.double().get_node("A").execute(), "Then we get the stubbed return value")
-
-func test_When_we_call_a_method_from_a_grandchild_node_that_we_stubbed():
-	describe("When we call a method from a grandchild node that we stubbed")
-
-	print("Testing When we call a method from a grandchild node that we stubbed")
-	director.get_node("C/D").method("wowsers").stub(99)
-
-	asserts.is_equal(99, director.double().get_node("C/D").wowsers(), "Then we get the stubbed return value")
+#func test_When_we_call_a_method_from_the_root_node_that_we_stubbed():
+#	describe("When we call a method from the root node that we stubbed")
+#
+#	print("Testing When we call a method from the root node that we stubbed")
+#	director.get_node(".").method("test").stub(9999)
+#
+#	asserts.is_equal(9999, director.double().get_node(".").test(), "Then we get the stubbed return value")
+#
+#func test_When_we_call_a_method_from_a_child_node_that_we_stubbed():
+#	describe("When we call a method from a child node that we stubbed")
+#
+#	print("Testing When we call a method from a chidl node that we stubbed")
+#	director.get_node("A").method("execute").stub(999)
+#
+#	asserts.is_equal(999, director.double().get_node("A").execute(), "Then we get the stubbed return value")
+#
+#func test_When_we_call_a_method_from_a_grandchild_node_that_we_stubbed():
+#	describe("When we call a method from a grandchild node that we stubbed")
+#
+#	print("Testing When we call a method from a grandchild node that we stubbed")
+#	director.get_node("C/D").method("wowsers").stub(99)
+#
+#	asserts.is_equal(99, director.double().get_node("C/D").wowsers(), "Then we get the stubbed return value")
 
