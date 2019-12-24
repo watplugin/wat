eace6ebd64533a928bd1c1d04ef9afdb74e818e0
CodeDarigan <code@darigan.ie>
2019-12-24 01:49:45 +0000

    Comment out the stub

--- tests/Bootstrapping/Integration/scene_director.test.gd
+++ tests/Bootstrapping/Integration/scene_director.test.gd
@@ -40,7 +40,7 @@ func test_When_we_call_a_method_from_a_child_node_that_we_stubbed():
 	describe("When we call a method from a child node that we stubbed")
 
 	print("Testing When we call a method from a chidl node that we stubbed")
-	director.get_node("A").method("execute").stub(999)
+	director.get_node("A").method("execute") # .stub(999)
 
 	asserts.is_true(true, "beep")
 #	asserts.is_equal(999, director.double().get_node("A").execute(), "Then we get the stubbed return value")
