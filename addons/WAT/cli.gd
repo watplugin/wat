24759584fce7e478d0124ea91e9957be86648111
CodeDarigan <code@darigan.ie>
2020-01-20 21:28:52 +0000

    Implement Basic CLI Scene

--- addons/WAT/cli.gd
+++ addons/WAT/cli.gd
@@ -1,90 +1,45 @@
-#extends SceneTree
-#
-## You can only call this from root. On Windows, write
-## godot -s addons/WAT/CLI.gd -run_all
-## to run all of the scripts
-#const RUNNER = preload("res://addons/WAT/runner/runner.gd")
-#const FILESYSTEM: Script = preload("res://addons/WAT/filesystem.gd")
-#const RUN_ALL: String = "-run_all"
-#const RUN_DIRECTORY: String = "-run_dir"
-#const RUN_SCRIPT: String = "-run_script"
-#const LIST_ALL: String = "-list_all"
-#const LIST_DIR: String = "-list_dir"
-#const PASSED: int = 0
-#const FAILED: int = 1
-#var start_time: float
-#
-#func _init():
-#	execute(arguments())
-#
-#func _default_directory() -> String:
-#	return ProjectSettings.get("WAT/Test_Directory")
-#
-#func arguments() -> Array:
-#	return Array(OS.get_cmdline_args()).pop_back().split("=")
-#
-#func execute(arguments: Array) -> void:
-#	var command: String = arguments.pop_front()
-#	match command:
-#		RUN_ALL:
-#			_run(_default_directory())
-#		RUN_DIRECTORY:
-#			_run(arguments.pop_front())
-#		RUN_SCRIPT:
-#			_run(arguments.pop_front())
-#		LIST_ALL:
-#			_list()
-#			quit()
-#		LIST_DIR:
-#			_list(arguments.pop_front())
-#			quit()
-#
-#func _run(directory: String) -> void:
-#	start_time = OS.get_ticks_msec()
-#	var Runner = RUNNER.new(FILESYSTEM)
-#	Runner.connect("ended", self, "display")
-#	root.add_child(Runner)
-#	Runner.name = Runner.MAIN
-#	Runner.run(directory)
-#
-#func _list(directory: String = "res://tests") -> void:
-#	print()
-#	print(FILESYSTEM.file_list(directory))
-#
-#func display(caselist: Array) -> void:
-#	var cases = {passed = 0, total = 0, crashed = 0}
-#	print("\n-------RESULTS-------")
-#	for case in caselist:
-#		cases.total += 1
-#		if case.crashed:
-#			display_crash(case)
-#			cases.crashed += 1
-#		elif case.success:
-#			cases.passed += 1
-#		else:
-#			display_failures(case)
-#	display_summary(cases)
-#	quit()
-#
-#
-#func display_failures(case) -> void:
-#	# Need to format paths to be accurate
-#	print("%s (%s)" % [case.title, case.path])
-#	for method in case.methods:
-#		if not method.success:
-#			print("\n  %s" % method.context)
-#			for expectation in method.expectations:
-#				if not expectation.success:
-#					print("\t%s" % expectation.context, "\n\t  (EXPECTED: %s) | (RESULT: %s)" % [expectation.expected, expectation.result])
-#
-#func display_summary(cases: Dictionary) -> void:
-#	print("\nTook %s ms" % str(OS.get_ticks_msec() - start_time))
-#	print("%s Tests Crashed" % cases.crashed)
-#	print("%s / %s Tests Passed" % [cases.passed, cases.total])
-#	print("-------RESULTS-------")
-#	OS.exit_code = PASSED if cases.total > 0 and cases.total == cases.passed and cases.crashed == 0 else FAILED
-#
-#func display_crash(case):
-#	print("CRASHED: %s (%s)" % [case.title, case.path])
-#	print("\n  (EXPECTED: %s) | (RESULT: %s)" % [case.crashdata.expected, case.crashdata.result])
-#
+extends Node
+
+const PASSED: int = 0
+const FAILED: int = 1
+const TestRunner: PackedScene = preload("res://addons/WAT/runner/TestRunner.tscn")
+const FileSystem: Reference = preload("res://addons/WAT/filesystem.gd")
+var _runner: Node = TestRunner.instance()
+var _start_time: float
+
+func _init() -> void:
+	WAT.DefaultConfig.test_loader.deposit(FileSystem.scripts("res://tests"))
+	_runner.connect("ended", self, "_on_testrunner_ended", [])
+	_start_time = OS.get_ticks_msec()
+	add_child(_runner)
+	print("Running WAT CLI!")
+
+func _on_testrunner_ended() -> void:
+	_runner.queue_free()
+	var caselist: Array = WAT.DefaultConfig.test_results.withdraw()
+	var cases = {passed = 0, total = 0, crashed = 0}
+	print("\n-------RESULTS-------")
+	for case in caselist:
+		cases.total += 1
+		if case.success:
+			cases.passed += 1
+		else:
+			display_failures(case)
+	display_summary(cases)
+
+func display_failures(case) -> void:
+	print("%s (%s)" % [case.context, case.path])
+	for method in case.methods:
+		if not method.success:
+			print("\n  %s" % method.context)
+			for assertion in method.assertions:
+				if not assertion.success:
+					print("\t%s" % assertion.context, "\n\t  (EXPECTED: %s) | (RESULTED: %s)" % [assertion.expected, assertion.actual])
+
+func display_summary(cases: Dictionary) -> void:
+	var seconds: float = (OS.get_ticks_msec() - _start_time) / 1000
+	print("\nTook %s seconds" % str(seconds))
+	print("%s Tests Crashed" % cases.crashed)
+	print("%s / %s Tests Passed" % [cases.passed, cases.total])
+	print("-------RESULTS-------")
+	OS.exit_code = PASSED if cases.total > 0 and cases.total == cases.passed and cases.crashed == 0 else FAILED
