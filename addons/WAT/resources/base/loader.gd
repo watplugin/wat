99861d0eb33c77db4d8bed79db12d323503be5b6
CodeDarigan <code@darigan.ie>
2020-01-22 17:06:19 +0000

    Fix TestSuiteOfSuites in Godot3.1.2/1

--- addons/WAT/resources/base/loader.gd
+++ addons/WAT/resources/base/loader.gd
@@ -22,8 +22,17 @@ func withdraw() -> Array:
 				if subtest.get("TEST") != null:
 					subtest.set_meta("path", "%s.%s" % [path, constant])
 					tests.append(subtest)
-		elif Engine.get_version_info().minor == 1:
-			push_warning("WAT.TestSuiteOfSuite only works in Godot 3.2 or higher")
+		elif test.get("IS_WAT_SUITE") and Engine.get_version_info().minor == 1:
+			var source = test.source_code
+			for l in source.split("\n"):
+				if l.begins_with("class"):
+					var classname = l.split(" ")[1]
+					var expr = Expression.new()
+					expr.parse(classname)
+					var subtest = expr.execute([], test)
+					if subtest.get("TEST") != null:
+						subtest.set_meta("path", "%s.%s" % [path, classname])
+						tests.append(subtest)
 	_tests = []
 	ResourceSaver.save(resource_path, self)
 	return tests
