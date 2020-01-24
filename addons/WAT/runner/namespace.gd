51994f6891e39b451998ea2f843b0613c277b7a3
CodeDarigan <code@darigan.ie>
2020-01-24 20:40:32 +0000

    Added Namespace Consts

--- addons/WAT/runner/namespace.gd
+++ addons/WAT/runner/namespace.gd
@@ -1,9 +1,12 @@
 extends Reference
 class_name WAT
 
+const TestDoubleFactory = preload("res://addons/WAT/double/factory.gd")
 const BaseTest = preload("base_test.gd")
 const Test: Script = preload("test.gd")
 const TestSuiteOfSuites = preload("suite.gd")
+const SignalWatcher = preload("res://addons/WAT/runner/watcher.gd")
+const Parameters = preload("res://addons/WAT/runner/parameters.gd")
 const Yielder = preload("yielder.gd")
 const Asserts = preload("res://addons/WAT/expectations/0_index.gd")
 const TestCase = preload("case.gd")
