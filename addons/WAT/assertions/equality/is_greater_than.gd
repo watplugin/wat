b5b1755e44837b42dec461fcf22b311bee2950ef
CodeDarigan <code@darigan.ie>
2020-01-26 14:03:06 +0000

    Move Equality Assertions into their own folder.

--- addons/WAT/assertions/equality/is_greater_than.gd
+++ addons/WAT/assertions/equality/is_greater_than.gd
@@ -1,11 +1,11 @@
-extends "base.gd"
+extends "../base.gd"
 
 func _init(a, b, context: String) -> void:
 	var typeofa = type2str(a)
 	var typeofb = type2str(b)
-	var passed: String = "|%s| %s <= |%s| %s" % [typeofa, a, typeofb, b]
-	var failed: String = "|%s| %s > |%s| %s" % [typeofa, a, typeofb, b]
+	var passed: String = "|%s| %s > |%s| %s" % [typeofa, a, typeofb, b]
+	var failed: String = "|%s| %s <= |%s| %s" % [typeofa, a, typeofb, b]
 	self.context = context
-	self.success = (a <= b)
+	self.success = (a > b)
 	self.expected = passed
 	self.result = passed if self.success else failed
