921c35e9b286551813df4710856d47c9684cf506
CodeDarigan <code@darigan.ie>
2020-01-26 14:27:05 +0000

    Move "is" keyword assertions to their own folder
    This was a biggie so we may have consumed some unrelated ones
    by accident.

---
+++ addons/WAT/assertions/boolean/is_not_equal.gd
@@ -0,0 +1,8 @@
+extends "../base.gd"
+
+
+func _init(a, b, context: String) -> void:
+	self.success = (a != b)
+	self.context = context
+	self.expected = "|%s| %s != |%s| %s" % [type2str(a), a, type2str(b), b]
+	self.result = "|%s| %s %s |%s| %s" % [type2str(a), a, ("!=" if self.success else "=="), type2str(b),b]
--- addons/WAT/assertions/is_not_equal.gd
+++
@@ -1,8 +0,0 @@
-extends "base.gd"
-
-
-func _init(a, b, context: String) -> void:
-	self.success = (a != b)
-	self.context = context
-	self.expected = "|%s| %s != |%s| %s" % [type2str(a), a, type2str(b), b]
-	self.result = "|%s| %s %s |%s| %s" % [type2str(a), a, ("!=" if self.success else "=="), type2str(b),b]
