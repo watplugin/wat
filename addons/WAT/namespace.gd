085a174b0a50236f78d9207b3337373a79023b2a
CodeDarigan <code@darigan.ie>
2020-01-26 13:32:31 +0000

    Move namespace up

--- addons/WAT/namespace.gd
+++ addons/WAT/namespace.gd
@@ -2,16 +2,16 @@ extends Reference
 class_name WAT
 
 const TestDoubleFactory = preload("res://addons/WAT/double/factory.gd")
-const BaseTest = preload("base_test.gd")
-const Test: Script = preload("test.gd")
+const BaseTest = preload("test/base_test.gd")
+const Test: Script = preload("test/test.gd")
 const TestLoader = preload("res://addons/WAT/test_runner/test_loader.gd")
 const Results: Resource = preload("res://addons/WAT/resources/results.tres")
-const TestSuiteOfSuites = preload("suite.gd")
+const TestSuiteOfSuites = preload("test/suite.gd")
 const SignalWatcher = preload("res://addons/WAT/test/watcher.gd")
 const Parameters = preload("res://addons/WAT/test/parameters.gd")
-const Yielder = preload("yielder.gd")
+const Yielder = preload("test/yielder.gd")
 const Asserts = preload("res://addons/WAT/assertions/0_index.gd")
-const TestCase = preload("case.gd")
+const TestCase = preload("test/case.gd")
 const Settings = preload("res://addons/WAT/settings.gd")
 
 class Icon:
