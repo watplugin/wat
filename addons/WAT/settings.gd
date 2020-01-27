61beef898bac9b4e71df194407e0c19fd84e2307
CodeDarigan <code@darigan.ie>
2020-01-27 19:45:31 +0000

    Add Print Debugs
    We have a segfault that happens between end() emit_signal("ended")
    and WAT.Settings.clear()
    
    My best guess is double-freeing a node. So we're adding a check to
    see if it is/isn't deleted before hand. If it segfaults and we
    see a false or deleted, then we can be sure trying to free something
    that was already freed was the problem.

--- addons/WAT/settings.gd
+++ addons/WAT/settings.gd
@@ -1,14 +1,5 @@
 extends Reference
 
-
-#
-#static func set_default_directory() -> void:
-#	var title: String = "WAT/Test_Directory"
-#	if not ProjectSettings.has_setting(title):
-#		var property_info: Dictionary = {"name": title, "type": TYPE_STRING, "hint_string": "Store your WATTests here"}
-#		ProjectSettings.set(title, "res://tests")
-#		ProjectSettings.add_property_info(property_info)
-#		push_warning("Set Test Directory to 'res://tests'. You can change this in Project -> Project Settings -> General -> WAT")
 const AUTO_QUIT: String = "WAT/AutoQuit"
 
 static func enable_autoquit() -> void:
@@ -28,4 +19,6 @@ static func test_directory() -> String:
 
 static func clear():
 	if ProjectSettings.has_setting("WAT/TestDouble"):
+		print(ProjectSettings.get_setting("WAT/TestDouble"))
+		print(is_instance_valid(ProjectSettings.get_setting("WAT/TestDouble")))
 		ProjectSettings.get_setting("WAT/TestDouble").queue_free()
