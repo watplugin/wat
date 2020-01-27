e86c7082c5c76d287001fbcd80044ea4fa6f1ae9
CodeDarigan <code@darigan.ie>
2020-01-27 20:01:02 +0000

    Print Debugs in Clear

--- addons/WAT/settings.gd
+++ addons/WAT/settings.gd
@@ -18,7 +18,12 @@ static func test_directory() -> String:
 	return ProjectSettings.get_setting("WAT/Test_Directory")
 
 static func clear():
+	print("21 clear")
 	if ProjectSettings.has_setting("WAT/TestDouble"):
+		print("23 clear")
 		print(ProjectSettings.get_setting("WAT/TestDouble"))
+		print("25 clear")
 		print(is_instance_valid(ProjectSettings.get_setting("WAT/TestDouble")))
+		print("27 clear")
 		ProjectSettings.get_setting("WAT/TestDouble").queue_free()
+		print("28 clear")
