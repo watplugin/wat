cf85db2704ec0a727a748c7b99b1786a92211926
unknown <code@darigan.ie>
2020-02-06 18:22:32 +0000

    Update Filesystem Ref in CLI

--- addons/WAT/ui/cli/cli.gd
+++ addons/WAT/ui/cli/cli.gd
@@ -9,7 +9,7 @@ const LIST_DIR: String = "-list_dir"
 const PASSED: int = 0
 const FAILED: int = 1
 const TestRunner: PackedScene = preload("res://addons/WAT/test_runner/TestRunner.tscn")
-const FileSystem: Reference = preload("res://addons/WAT/filesystem.gd")
+const FileSystem: Reference = preload("res://addons/WAT/system/filesystem.gd")
 var _runner: Node
 var _start_time: float
 
