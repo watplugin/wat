1184d8c3b408acd0ec9e1d65233adf7e692bf567
unknown <code@darigan.ie>
2020-02-06 17:08:01 +0000

    Replace Metadata With Resource
    We ran into serialization issues with metadata (it
    was never being saved). So I've decided to use a resource
    instead.
    
    We can't give a script type hint to a dictionary which caused
    other issues with caching scripts. So instead we use two arrays that
    share data via index (script at X in scripts owns that tags at X in
    tags).
    
    Only thing left now is to run some tagged tests.

--- addons/WAT/ui/metadata/metadata.gd
+++ addons/WAT/ui/metadata/metadata.gd
@@ -1,101 +1,75 @@
 tool
 extends Control
 
-# TODO
-# Implement Run Tags
-# Implement Deleting Tags
-# Add Counts
-# OptionButton changes on selector
+# Check Tags
+# Add Tag (Only if not already)
+# Delete Tag
+# ON LOAD DISPLAY ALL RELEVANT TAGS
+# CLEAR OUT ALL TAGS
+var test: Resource
+const Index: Resource = preload("index.tres")
+const Tag: PackedScene = preload("Tag.tscn")
+onready var TagSelector: Control = $Tags/Select.get_popup()
+onready var TagList: Control = $TagList
 
-var test: Script 
-onready var TagArray: Control = $Main/TagArray
-onready var Options: Control = $Main/Options
-onready var ShowTags: Button = $Main/Options/Button
-onready var TagSize: Label = $Main/TagArray/Size
-onready var TagSizer: SpinBox = $Main/TagArray/SpinBox
-var _cache: Array
-var _tags: PoolStringArray = []
-var _count: int = 0
+func global_tags() -> Array:
+	# Add this to settings?
+	return ProjectSettings.get_setting("WAT/Tags")
 
+func tags() -> Array:
+	# May need to add error handling here
+	return Index.tags[id()]
+	
+func id() -> int:
+	# returns position in scripts array
+	return Index.scripts.find(test)
+	
 func _ready() -> void:
-	ShowTags.connect("pressed", self, "_show_tags")
-	TagSizer.connect("value_changed", self, "_set_sizes")
-	add_tags()
+	$Data/ClearAll.connect("pressed", self, "_delete_all")
+	test = load(test.resource_path) # I think this gives us the binary path?
+	if not Index.scripts.has(test):
+		Index.scripts.append(test)
+		Index.tags.append([])
+		save()
+	populate()
+	TagSelector.connect("about_to_show", self, "_update_selectable_tags")
+	TagSelector.connect("id_pressed", self, "_add_tag")
 	
-func add_tags() -> void:
-	if not test.has_meta("Tags"):
-		test.set_meta("Tags", [])
-		return
-	print(test.get_meta("Tags"))
-	_tags = test.get_meta("Tags")
-	for tag in _tags:
-		_add_tag(tag)
-	ShowTags.text = "Array (size %s)" % _count as String
-	TagSizer.value = _count
-	if _count > 0:
-		TagArray.visible = true
+func _delete_all() -> void:
+	for child in TagList.get_children():
+		delete(child)
+	
+func populate() -> void:
+	for t in tags():
+		var tag: Control = Tag.instance()
+		TagList.add_child(tag)
+		tag.Name.text = t
+		tag.Delete.connect("pressed", self, "delete", [tag])
 		
-func _set_sizes(value: int) -> void:
-	_count = 0
-	clear_cache()
-	for i in value:
-		# if current index is greater than tags
-		if i > _tags.size() - 1:
-			# add default tag
-			_add_tag()
-		else:
-			# add tag from index
-			_add_tag(_tags[i])
+func _update_selectable_tags() -> void:
+	TagSelector.clear()
+	for tag in global_tags():
+		if not tag as String in tags():
+			TagSelector.add_item(tag)
+	TagSelector.update()
 			
-func clear_cache() -> void:
-	for item in _cache:
-		item.get_parent().remove_child(item)
-		item.queue_free()
+func _add_tag(id: int) -> void:
+	var tagtext: String = TagSelector.get_item_text(id)
+	var tag: Control = Tag.instance()
+	TagList.add_child(tag)
+	tag.Name.text = tagtext
+	tag.Delete.connect("pressed", self, "delete", [tag])
+	Index.tags[id()].append(tagtext as String)
+	save()
 	
-func _add_tag(tag: String = "Untagged") -> void:
-	var count = Label.new()
-	var taglabel = OptionButton.new()
-	taglabel.align = OptionButton.ALIGN_CENTER
-	taglabel.action_mode = Button.ACTION_MODE_BUTTON_RELEASE
-	expand(count)
-	expand(taglabel)
-	taglabel.connect("pressed", self, "_populate_menu", [taglabel])
-	taglabel.connect("item_selected", self, "_save_tag", [taglabel])
-	_count += 1
-	count.text = _count as String
-	taglabel.text = tag
-	_cache.append(count)
-	_cache.append(taglabel)
-	TagArray.add_child(count)
-	TagArray.add_child(taglabel)
-
-func _save_tag(id: int, taglabel: OptionButton) -> void:
-	if taglabel.get_item_text(id) == "Untagged":
-		push_warning("Invalid")
-		return
-	test.get_meta("Tags").append(taglabel.get_item_text(id))
-	print(test.get_meta("Tags"))
-	push_warning("Saving Not Implemented")
-	
-func expand(control: Control) -> void:
-	control.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	control.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	
-func _populate_menu(menu: OptionButton) -> void:
-	menu.clear()
-	menu.add_item("Tag")
-	for tag in ProjectSettings.get_setting("WAT/Tags"):
-		if not tag in test.get_meta("Tags"):
-			var data = {"tag": tag, "tags": test.get_meta("Tags")}
-			print("{tag} is not in {tags}".format(data))
-			menu.add_item(tag)
-	
-func _show_tags() -> void:
-	TagArray.visible = not TagArray.visible
-
-"""
-0) Click Array Button -> Hides/Shows TagArray
-1) On Load: Define Tag Array Size and Show
-2) On Load: Populate With Tags via Metadata (on click instead?)
-3) On MenuButton Clicked: Show Tag Options EXCEPT already selected
-"""
+func delete(tag: Control) -> void:
+	print("deleting")
+	TagList.remove_child(tag)
+	tag.queue_free()
+#	print(Index.tags[id()].has(tag.Name.))
+	Index.tags[id()].erase(tag.Name.text as String)
+	save()
+#
+func save() -> void:
+	var err = ResourceSaver.save(Index.resource_path, Index)
+	push_warning(err as String)
