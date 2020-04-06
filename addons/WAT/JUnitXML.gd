8d79469797e409f4ef05d3fec6589974c5768a93
unknown <code@darigan.ie>
2020-04-06 19:32:50 +0100

    Fix Typoo

--- addons/WAT/JUnitXML.gd
+++ addons/WAT/JUnitXML.gd
@@ -16,7 +16,7 @@ func save(results, time: float = 0.0) -> void:
 				output += '\n<testcase name="%s" time="%s">' % [case.context, case.time]
 				for assertion in case.assertions:
 					if not assertion.success:
-						output += '\n<failure message="%s"> EXPECTED: %S | GOT %s</failure/' % [assertion.context, assertion.expected, assertion.actual]
+						output += '\n<failure message="%s"> EXPECTED: %s | GOT %s</failure/' % [assertion.context, assertion.expected, assertion.actual]
 				output += '\n</testcase>' 
 		output += "\n</testsuite>"
 	output += '\n</testsuites>'
@@ -24,12 +24,3 @@ func save(results, time: float = 0.0) -> void:
 	XML.open("res://tests/results/results.xml", File.WRITE)
 	XML.store_string(output)
 	XML.close()
-
-#func display_failures(case) -> void:
-#	print("%s (%s)" % [case.context, case.path])
-#	for method in case.methods:
-#		if not method.success:
-#			print("\n  %s" % method.context)
-#			for assertion in method.assertions:
-#				if not assertion.success:
-#					print("\t%s" % assertion.context, "\n\t  (EXPECTED: %s) | (RESULTED: %s)" % [assertion.expected, assertion.actual])
