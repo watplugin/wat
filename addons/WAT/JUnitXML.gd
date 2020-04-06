cfcff87306daf66f3048c0b4cb3fb44bf6e31622
unknown <code@darigan.ie>
2020-04-06 19:34:32 +0100

    Fix Markup

--- addons/WAT/JUnitXML.gd
+++ addons/WAT/JUnitXML.gd
@@ -10,13 +10,13 @@ func save(results, time: float = 0.0) -> void:
 	output += '<?xml version="1.0" ?>'
 	output += '\n<testsuites failures="%s" name="TestXML" tests="%s" time="%s">' % [failures, tests, time]
 	for result in results:
-		output += '\n<testsuite failures="%s" name="%s" tests="%s" time= "%s">' % [result.total - result.passed, result.context, result.total, result.time_taken]
+		output += '\n<testsuite failures="%s" name="%s" tests="%s" time="%s">' % [result.total - result.passed, result.context, result.total, result.time_taken]
 		for case in result.methods:
 			if not case.success:
 				output += '\n<testcase name="%s" time="%s">' % [case.context, case.time]
 				for assertion in case.assertions:
 					if not assertion.success:
-						output += '\n<failure message="%s"> EXPECTED: %s | GOT %s</failure/' % [assertion.context, assertion.expected, assertion.actual]
+						output += '\n<failure message="%s"> EXPECTED: %s | GOT %s</failure>' % [assertion.context, assertion.expected, assertion.actual]
 				output += '\n</testcase>' 
 		output += "\n</testsuite>"
 	output += '\n</testsuites>'
