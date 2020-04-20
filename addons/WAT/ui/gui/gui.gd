ae339054ed6b2899e6af62d3b12c55257e901bf4
unknown <code@darigan.ie>
2020-04-21 00:19:58 +0100

    Pull Up UI Scripts To Top UI Node

--- addons/WAT/ui/gui/gui.gd
+++ addons/WAT/ui/gui/gui.gd
@@ -1,18 +1,18 @@
 tool
 extends VBoxContainer
 
-enum RESULTS { EXPAND_ALL, COLLAPSE_ALL, EXPAND_FAILURES }
-onready var Interact: HBoxContainer = $Interact
-onready var Summary: Label = $Summary
-onready var Results: TabContainer = $Results
-var filesystem
-
-func _on_view_pressed(id: int) -> void:
-	match id:
-		RESULTS.EXPAND_ALL:
-			Results.expand_all()
-		RESULTS.COLLAPSE_ALL:
-			Results.collapse_all()
-		RESULTS.EXPAND_FAILURES:
-			Results.expand_failures()
+#enum RESULTS { EXPAND_ALL, COLLAPSE_ALL, EXPAND_FAILURES }
+#onready var Interact: HBoxContainer = $Interact
+#onready var Summary: Label = $Summary
+#onready var Results: TabContainer = $Results
+#var filesystem
+#
+#func _on_view_pressed(id: int) -> void:
+#	match id:
+#		RESULTS.EXPAND_ALL:
+#			Results.expand_all()
+#		RESULTS.COLLAPSE_ALL:
+#			Results.collapse_all()
+#		RESULTS.EXPAND_FAILURES:
+#			Results.expand_failures()
 
--- addons/WAT/ui/gui/wat.gd
+++ addons/WAT/ui/gui/wat.gd
@@ -1,17 +1,55 @@
 tool
 extends PanelContainer
 
+enum RESULTS { EXPAND_ALL, COLLAPSE_ALL, EXPAND_FAILURES }
 enum RUN { ALL, DIRECTORY, SCRIPT, TAGGED }
 const NOTHING_SELECTED: int = -1
+const filesystem = preload("res://addons/WAT/system/filesystem.gd")
 const TestRunner: String = "res://addons/WAT/test_runner/TestRunner.tscn"
 onready var GUI: VBoxContainer = $GUI
+onready var Interact: HBoxContainer = $GUI/Interact
+onready var Summary: Label = $GUI/Summary
+onready var Results: TabContainer = $GUI/Results
+onready var Run: HBoxContainer = $GUI/Interact/Run
+onready var Select: HBoxContainer = $GUI/Interact/Select
+onready var ViewMenu: MenuButton = $GUI/Interact/View.get_popup()
+onready var QuickStart: Button = $GUI/Interact/Run/QuickStart
+onready var Menu: MenuButton = $GUI/Interact/Run/Menu.get_popup()
+onready var DirectorySelector: OptionButton = $GUI/Interact/Select/Directory
+onready var ScriptSelector: OptionButton = $GUI/Interact/Select/Script
+onready var TagSelector: OptionButton = $GUI/Interact/Select/Tag
 var execute = preload("res://addons/WAT/execute.gd").new()
+
+func _on_view_pressed(id: int) -> void:
+	match id:
+		RESULTS.EXPAND_ALL:
+			Results.expand_all()
+		RESULTS.COLLAPSE_ALL:
+			Results.collapse_all()
+		RESULTS.EXPAND_FAILURES:
+			Results.expand_failures()
 
 func _ready() -> void:
 	set_process(false)
-	GUI.Interact.Run.QuickStart.connect("pressed", self, "_on_run_pressed", [RUN.ALL])
-	GUI.Interact.Run.Menu.connect("id_pressed", self, "_on_run_pressed")
-	GUI.Interact.ViewMenu.connect("id_pressed", GUI.Results, "_on_view_pressed")
+	Menu.clear()
+	Menu.add_item("Run All Tests")
+	Menu.add_item("Run Selected Directory")
+	Menu.add_item("Run Selected Script")
+	Menu.add_item("Run Tagged")
+	ViewMenu.clear()
+	ViewMenu.add_item("Expand All Results")
+	ViewMenu.add_item("Collapse All Results")
+	ViewMenu.add_item("Expand All Failures")
+	QuickStart.connect("pressed", self, "_on_run_pressed", [RUN.ALL])
+	Menu.connect("id_pressed", self, "_on_run_pressed")
+	ViewMenu.connect("id_pressed", $GUI/Results, "_on_view_pressed")
+	DirectorySelector.clear()
+	DirectorySelector.add_item("Select Directory")
+	ScriptSelector.add_item("Select Script")
+	TagSelector.add_item("Select Tag")
+	DirectorySelector.connect("pressed", self, "_on_directory_selector_pressed")
+	ScriptSelector.connect("pressed", self, "_on_script_selector_pressed")
+	TagSelector.connect("pressed", self, "_on_tag_selector_pressed")
 
 func _on_run_pressed(option: int) -> void:
 	set_process(true)
@@ -44,3 +82,22 @@ func selected(selector: OptionButton) -> String:
 		push_warning("Nothing Selected")
 	return selector.get_item_text(selector.selected)
 
+func _on_directory_selector_pressed() -> void:
+	DirectorySelector.clear()
+	DirectorySelector.add_item(ProjectSettings.get_setting("WAT/Test_Directory"))
+	for directory in filesystem.directories():
+		DirectorySelector.add_item(directory)
+		
+func _on_script_selector_pressed() -> void:
+	ScriptSelector.clear()
+	for script in filesystem.scripts():
+		if script.ends_with(".gd"):
+			if load(script).get("TEST") != null:
+				ScriptSelector.add_item(script)
+			if load(script).get("IS_WAT_SUITE"):
+				ScriptSelector.add_item(script)
+				
+func _on_tag_selector_pressed() -> void:
+	TagSelector.clear()
+	for tag in ProjectSettings.get_setting("WAT/Tags"):
+		TagSelector.add_item(tag)
