2ec033c4cf53867c412e3d6a5dfc0e179e4d79ea
unknown <code@darigan.ie>
2020-04-23 16:17:07 +0100

    Rename Plugin Properties

--- addons/WAT/plugin.gd
+++ addons/WAT/plugin.gd
@@ -2,25 +2,27 @@ tool
 extends EditorPlugin
 
 const TITLE: String = "Tests"
-const UI: PackedScene = preload("res://addons/WAT/Wat.tscn")
+const ControlPanel: PackedScene = preload("res://addons/WAT/Wat.tscn")
 const TestMetadataEditor: Script = preload("res://addons/WAT/ui/metadata/exports.gd")
-var interface: PanelContainer
-var test_metadata_editor: EditorInspectorPlugin
-var Dock: Node
+const DockController: Script = preload("system/dock.gd")
+
+var _ControlPanel: PanelContainer
+var _TestMetadataEditor: EditorInspectorPlugin
+var _DockController: Node
 
 func get_plugin_name() -> String:
    return "WAT"
 
 func _enter_tree() -> void:
-	interface = UI.instance()
-	test_metadata_editor = TestMetadataEditor.new()
+	_ControlPanel = ControlPanel.instance()
+	_TestMetadataEditor = TestMetadataEditor.new()
 	WAT.Settings.add_templates()
-	Dock = WAT.Settings.Dock.new(self, interface)
-	add_child(Dock)
-	add_inspector_plugin(test_metadata_editor)
+	_DockController = DockController.new(self, _ControlPanel)
+	add_child(_DockController)
+	add_inspector_plugin(_TestMetadataEditor)
 	WAT.Settings.set_minimize_on_load()
 	
 func _exit_tree() -> void:
-	Dock.free()
-	interface.free()
-	remove_inspector_plugin(test_metadata_editor)
+	_DockController.free()
+	_ControlPanel.free()
+	remove_inspector_plugin(_TestMetadataEditor)
