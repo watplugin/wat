8414109be2444b6eb85381009c0c51e20843ddf4
unknown <code@darigan.ie>
2020-04-23 15:23:58 +0100

    Move Test Folder To Core

--- addons/WAT/namespace.gd
+++ addons/WAT/namespace.gd
@@ -2,16 +2,16 @@ extends Reference
 class_name WAT
 
 const TestDoubleFactory = preload("res://addons/WAT/core/double/factory.gd")
-const BaseTest = preload("res://addons/WAT/test/base_test.gd")
-const Test: Script = preload("res://addons/WAT/test/test.gd")
+const BaseTest = preload("res://addons/WAT/core/test/base_test.gd")
+const Test: Script = preload("res://addons/WAT/core/test/test.gd")
 #const TestLoader = preload("res://addons/WAT/test_runner/test_loader.gd")
 const Results: Resource = preload("res://addons/WAT/resources/results.tres")
-const TestSuiteOfSuites = preload("res://addons/WAT/test/suite.gd")
-const SignalWatcher = preload("res://addons/WAT/test/watcher.gd")
-const Parameters = preload("res://addons/WAT/test/parameters.gd")
-const Yielder = preload("res://addons/WAT/test/yielder.gd")
+const TestSuiteOfSuites = preload("res://addons/WAT/core/test/suite.gd")
+const SignalWatcher = preload("res://addons/WAT/core/test/watcher.gd")
+const Parameters = preload("res://addons/WAT/core/test/parameters.gd")
+const Yielder = preload("res://addons/WAT/core/test/yielder.gd")
 const Asserts = preload("res://addons/WAT/assertions/assertions.gd")
-const TestCase = preload("res://addons/WAT/test/case.gd")
+const TestCase = preload("res://addons/WAT/core/test/case.gd")
 const Settings = preload("res://addons/WAT/system/settings.gd")
 
 class Icon:
