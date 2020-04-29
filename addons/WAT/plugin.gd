6cbe52b8a727c6eea5a10b02d0b546daa6b61112
unknown <code@darigan.ie>
2020-04-29 12:15:22 +0100

    Move Plugin Around
    We need to initialize the metadata editor before we do the WAT gui
    otherwise the tag system isn't ready.

--- addons/WAT/plugin.gd
+++ addons/WAT/plugin.gd
@@ -18,9 +18,10 @@ func _enter_tree() -> void:
 	SystemInitializer.new()
 	_ControlPanel = ControlPanel.instance()
 	_TestMetadataEditor = TestMetadataEditor.new()
+	add_inspector_plugin(_TestMetadataEditor)
 	_DockController = DockController.new(self, _ControlPanel)
 	add_child(_DockController)
-	add_inspector_plugin(_TestMetadataEditor)
+
 	
 func _exit_tree() -> void:
 	_DockController.free()
