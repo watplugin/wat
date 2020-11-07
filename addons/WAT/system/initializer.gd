888d082aefa7b1cbe9464a1a0a8b8479c6518914
AlexAbdDraw <darigan@protonmail.com>
2020-11-07 11:09:01 +0000

    Remove Script Templates.
    Unnecessary and unhelpful. Usually just gets in the way.

--- addons/WAT/system/initializer.gd
+++ addons/WAT/system/initializer.gd
@@ -3,7 +3,6 @@ extends Reference
 func _init() -> void:
 	_create_test_folder()
 	_create_results_folder()
-	_add_script_templates()
 	_add_window_setting()
 	_add_test_strategy_setting()
 	_add_test_metadata_folder()
@@ -50,18 +49,6 @@ func _create_results_folder() -> void:
 		ProjectSettings.set(title, "res://tests/results/WAT")
 		ProjectSettings.add_property_info(property_info)
 		push_warning("Set Result Directory to 'res://tests/results/WAT'. You can change this in Project -> Project Settings -> General -> WAT")
-
-func _add_script_templates() -> void:
-	var data = preload("res://addons/WAT/system/filesystem.gd").templates()
-	if data.exists:
-		return
-	var path = ProjectSettings.get_setting("editor/script_templates_search_path")
-	var wat_template = load("res://addons/WAT/core/test/template.gd")
-	var savepath: String = "%s/wat.test.gd" % path
-	if Directory.new().dir_exists(savepath):
-		return
-	ResourceSaver.save(savepath, wat_template)
-	push_warning("Added WAT Script Template to %s" % path)
 	
 func _add_window_setting() -> void:
 	if not ProjectSettings.has_setting("WAT/Minimize_Window_When_Running_Tests"):
