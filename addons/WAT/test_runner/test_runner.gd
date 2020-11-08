d140b47a88d3286678c7aea10fa3ef645c6d44db
AlexAbdDraw <darigan@protonmail.com>
2020-11-08 02:39:09 +0000

    Remove Speculative Comments

--- addons/WAT/test_runner/test_runner.gd
+++ addons/WAT/test_runner/test_runner.gd
@@ -14,25 +14,6 @@ var strategy: Reference = preload("res://addons/WAT/test_runner/strategy.gd").ne
 var test_double_registry: Node = preload("res://addons/WAT/double/registry.gd").new()
 var time_taken: float
 
-signal TestScriptCompleted;
-var EditorTalk: Node # Communication between game & Editor, delegates info via signals
-var TestLoader: Node # # Uses FileSystem to search and parse tests
-var TestStrategy: Node # Searchs and returns test strategy + related parameters
-# var TestFactory: Node # Creates instance from script in loader (avoids construction inside test)
-var Results: Node # Stores and Serializes Results
-
-#func initialize_REFACTORED() -> void:
-#	TestFactory.add_test_scripts(TestLoader.load_tests(TestStrategy))
-#
-#func run_REFACTORED() -> void:
-#	while TestFactory.can_create_tests:
-#		var test = TestFactory.create_test(TestLoader.next_script())
-#		add_child(test)
-#		yield(test.run(), "TestScriptCompleted")
-#		Results.add(test)
-#		remove_child(test)
-	
-	
 func _ready() -> void:
 	_time = OS.get_ticks_msec()
 	if get_tree().root.get_child(0) == self:
