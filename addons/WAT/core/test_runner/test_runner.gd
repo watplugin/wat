533479926da69ddfb43a6bb4de06535e7187d128
Alex <code@darigan.ie>
2021-01-04 22:21:20 +0000

    TestRunner runs Threaded Tests

--- addons/WAT/core/test_runner/test_runner.gd
+++ addons/WAT/core/test_runner/test_runner.gd
@@ -1,30 +1,51 @@
 extends Node
 
+const LOCKED = true
 signal finished
 export(Script) var TestController
 export(Array, Dictionary) var tests = []
+export(int) var threads = 4
+var running_tests: Array = []
 var is_editor: bool = true
 var results = []
 var _cursor: int = 0
+var controllers: Dictionary = {}
+
 # In Threaded Versions, we'll create a controller per thread
-var test_controller
+#var test_controller
 
 func _ready() -> void:
-	test_controller = TestController.new()
-	add_child(test_controller)
+	for thread in threads:
+		var controller = TestController.new()
+		controllers[controller] = Thread.new()
+		controller.connect("finished", self, "_on_controller_finished", [controller])
+		add_child(controller)
 	print("Initializing TestRunner")
 	_run()
-
-func _run() -> void:
-	# In Threaded versions, we could replace this with a system in process using "isRunning" boolean
-	while not _cursor == tests.size():
-		test_controller.run(tests[_cursor])
-		_cursor += 1
-		yield(test_controller, "finished")
-		results.append(test_controller.results)
-	WAT.results().save(results)
-	_terminate()
 	
+func _process(delta) -> void:
+	_run()
+	
+func _on_controller_finished(controller) -> void:
+	controllers[controller].wait_to_finish()
+	results.append(controller.results)
+	
+func _run() -> void:
+	# Controller passes itself in via signal when it is ready for another test
+#	if _cursor < tests.size() - 1:
+	if not tests.empty():
+		for controller in controllers:
+			# If a Thread is free
+			if not controllers[controller].is_active():
+				var test = tests.pop_front()
+				controllers[controller].start(controller, "run", test)
+	else:
+		for controller in controllers:
+			if controllers[controller].is_active():
+				return
+		_terminate()
+
 func _terminate() -> void:
+	WAT.results().save(results)
 	print("Terminating TestRunner")
 	get_tree().quit() if is_editor else emit_signal("finished")
