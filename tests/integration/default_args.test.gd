2bc88f8ecb8efd1d3459133a246a24928cb81e1a
Alex <code@darigan.ie>
2021-01-06 16:00:30 +0000

    ReAdd Tests

--- tests/integration/default_args.test.gd
+++ tests/integration/default_args.test.gd
@@ -8,7 +8,6 @@ func test_default_arguments_of_interpolate_property():
 	
 	var tween_director = direct.script("Tween")
 	var interpolate_method = tween_director.method("interpolate_property")
-	#asserts.is_true(ClassDB.class_exists("Tween"))
 	var double = tween_director.double()
 	asserts.is_true(true, 
 	 "If this method can be reached then we have doubled a script with default argument(s) properly!")
