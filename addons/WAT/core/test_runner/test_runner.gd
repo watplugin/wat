fb2ba589c472388acc41ba8e780359dd0ff41a4b
Alex <code@darigan.ie>
2021-01-24 19:01:32 +0000

    Add Multithreaded Runner

--- addons/WAT/core/test_runner/test_runner.gd
+++ addons/WAT/core/test_runner/test_runner.gd
@@ -2,89 +2,29 @@ extends Node
 
 onready var SingleThreadedRunner: Node = $SingleThreadedRunner
 onready var MultiThreadedRunner: Node = $MultiThreadedRunner
-
-signal finished
-export(Script) var TestController
 export(Array, Dictionary) var tests = []
-export(int) var threads = 7 # 1 thread = single_core, multi_thread starts at 2
-var running_tests: Array = []
+export(int) var threads = 7
 var is_editor: bool = true
-var results = []
-var _cursor: int = 0
-var controllers: Dictionary = {}
-var _is_terminating = false
-var _test_controller
+signal finished
 
 func _ready() -> void:
+	threads = _validate_threads(threads)
+	if threads > 1:
+		MultiThreadedRunner.connect("run_completed", self, "_on_run_completed")
+		MultiThreadedRunner.run(tests, threads)
+	else:
+		SingleThreadedRunner.connect("run_completed", self, "_on_run_completed")
+		SingleThreadedRunner.run(tests)
+		
+func _on_run_completed(results: Array) -> void:
+	WAT.ResManager.results().save(results)
+	print("Terminating TestRunner")
+	get_tree().quit() if is_editor else emit_signal("finished")
+	
+func _validate_threads(threads: int) -> int:
 	if threads == OS.get_processor_count():
 		push_warning("Max Available Threads is %s" % (OS.get_processor_count() - 1) as String)
 		threads = OS.get_processor_count() - 1
 		if threads == 0: # Unlikely event that people only have a single thread?
 			threads = 1
-	print("Initializing TestRunner")
-	_is_terminating = false
-	controllers = {}
-	if threads > 1:
-		for thread in threads:
-			var controller = TestController.new()
-			controllers[controller] = Thread.new()
-			controller.connect("finished", self, "_on_controller_finished", [controller])
-			add_child(controller)
-		call_deferred("_run_threaded")
-	else:
-		SingleThreadedRunner.connect("run_completed", self, "_on_run_completed")
-		SingleThreadedRunner.run(tests)
-#		_test_controller = TestController.new()
-#		add_child(_test_controller)
-#		_run_single_thread()
-		
-func _on_run_completed(_results: Array) -> void:
-	results = _results
-	_terminate()
-	
-func _on_controller_finished(controller) -> void:
-	controllers[controller].wait_to_finish()
-	results.append(controller.results)
-	call_deferred("_run_threaded")
-	
-#func _run_single_thread():
-#	# In Threaded versions, we could replace this with a system in process using "isRunning" boolean
-#	while not tests.empty():
-#		_test_controller.run(tests.pop_front())
-#		yield(_test_controller, "finished")
-#		results.append(_test_controller.results)
-#	_terminate()
-	
-func _run_threaded() -> void:
-	if threads == 1:
-		return
-	if not tests.empty():
-		for controller in controllers:
-			# If a Thread is free
-			if not controllers[controller].is_active():
-				var test = tests.pop_front()
-				controllers[controller].start(controller, "run", test)
-	else:
-		for controller in controllers:
-			if controllers[controller].is_active():
-				return
-		_terminate()
-		
-func _process(delta):
-	_run_threaded()
-
-func _terminate() -> void:
-	for controller in controllers:
-		controllers.erase(controller)
-		remove_child(controller)
-		controller.delete()
-		controller.queue_free()
-	if _test_controller != null:
-		_test_controller.delete()
-		_test_controller.queue_free()
-	if _is_terminating and threads > 1:
-		return
-	_is_terminating = true
-	WAT.ResManager.results().save(results)
-	print("Terminating TestRunner")
-	get_tree().quit() if is_editor else emit_signal("finished")
+	return threads
