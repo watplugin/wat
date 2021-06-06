137d435585e9c73404db9cb7463744dd54942d64
Alex <47497501+AlexDarigan@users.noreply.github.com>
2021-06-06 13:47:12 +0100

    Removed Debug Prints

--- addons/WAT/runner/TestRunner.gd
+++ addons/WAT/runner/TestRunner.gd
@@ -1,6 +1,7 @@
 tool
 extends Node
 
+const Settings: GDScript = preload("res://addons/WAT/settings.gd")
 const Splitter: GDScript = preload("splitter.gd")
 const COMPLETED: String = "completed"
 signal completed
@@ -8,31 +9,27 @@ signal completed
 func _ready() -> void:
 	name = "TestRunner"
 	if not Engine.is_editor_hint():
-		OS.window_size = ProjectSettings.get_setting("WAT/Window_Size")
-		OS.window_minimized = ProjectSettings.get_setting("WAT/Minimize_Window_When_Running_Tests")
+		OS.window_size = Settings.window_size()
+		OS.window_minimized = Settings.minimize_window_when_running_tests()
 
 func run(tests, threads) -> void:
 	var results: Array = []
 	var testthreads = Splitter.split(tests, threads)
-	print("Threads?", threads)
-	print("Testthreads", testthreads.size())
 	for thread in testthreads:
-		print("Running Thread")
 		add_child(thread.controller)
 		thread.start(self, "_run", thread)
 		thread.wait_to_finish()
 	for count in testthreads:
-		print("yielding for testthreads")
 		results += yield(self, COMPLETED)
 	return results
 
 	
-func _run(thread) -> void:
-	print("_Running thread")
-	print("Tests size? ", thread.tests.size())
+func _run(thread: Thread) -> void:
 	var results: Array = []
 	for test in thread.tests:
 		results.append(yield(thread.controller.run(test), COMPLETED))
-	print("Returned from controller")
 	thread.controller.queue_free()
 	emit_signal(COMPLETED, results)
+
+
+
