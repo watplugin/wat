450ea647815b9c40bf50a8e170ac142ea6edd00f
Alex <47497501+AlexDarigan@users.noreply.github.com>
2021-06-26 16:36:17 +0100

    Add X

---
+++ addons/WAT/filesystem/index.gd
@@ -0,0 +1,9 @@
+tool
+extends Resource
+
+# 1) Used To Store Directory Information
+# 2) Is used as a live resource
+# 3) Save on quit
+# 4) Load on start
+# 5) Contains metadata (tag info)
+export(Script) var s
