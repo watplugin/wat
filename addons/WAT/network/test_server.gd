e346ff04b7257fa5c4572712ca419f146bcc2f63
Lemoney <railkill@gmail.com>
2021-11-29 03:44:24 +0800

    Fix AlexDarigan/WAT#316 GUI debug endless yield on crash

--- addons/WAT/network/test_server.gd
+++ addons/WAT/network/test_server.gd
@@ -1,36 +1,51 @@
 tool
 extends "res://addons/WAT/network/test_network.gd"
 
-enum STATE { SENDING, RECEIVING }
 signal network_peer_connected
 signal results_received
+
+enum STATE { SENDING, RECEIVING, DISCONNECTED }
+
 var _peer_id: int
+# Store incoming cases from client in case of abrupt termination.
+var caselist: Array = []
+var results_view: TabContainer
+var status: int = STATE.DISCONNECTED
 
 func _ready() -> void:
 	if not Engine.is_editor_hint():
 		return
 	custom_multiplayer.connect("network_peer_connected", self, "_on_network_peer_connected")
+	custom_multiplayer.connect("network_peer_disconnected", self, "_on_network_peer_disconnected")
 	if _error(_peer.create_server(PORT, MAXCLIENTS)) == OK:
 		custom_multiplayer.network_peer = _peer
 	
 func _on_network_peer_connected(id: int) -> void:
 	_peer_id = id
-	_peer.set_peer_timeout(id, 59000, 60000, 61000)
+	_peer.set_peer_timeout(id, 1000, 2000, 3000)
 	emit_signal("network_peer_connected")
-	
+
+func _on_network_peer_disconnected(_id: int) -> void:
+	if status == STATE.SENDING:
+		emit_signal("results_received", caselist)
+	caselist.clear()
+	status = STATE.DISCONNECTED
+
 func send_tests(testdir: Array, repeat: int, thread_count: int) -> void:
+	status = STATE.SENDING
 	rpc_id(_peer_id, "_on_tests_received_from_server", testdir, repeat, thread_count)
 
 master func _on_results_received_from_client(results: Array = []) -> void:
+	status = STATE.RECEIVING
 	emit_signal("results_received", results)
 	_peer.disconnect_peer(_peer_id, true)
-	
-var results_view: TabContainer
+
 master func _on_test_script_started(data: Dictionary) -> void:
 	results_view.on_test_script_started(data)
 	
 master func _on_test_script_finished(data: Dictionary) -> void:
 	results_view.on_test_script_finished(data)
+	caselist.append(data)
 
 master func _on_test_method_started(data: Dictionary) -> void:
 	results_view.on_test_method_started(data)
